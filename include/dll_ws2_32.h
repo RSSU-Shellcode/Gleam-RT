d2b417fe06ea22ce5a141c391f6f8c9a87da65af
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-22 13:44:20 +0800

    dll_ws2_32.h: move macro and typedef about WSA from windows_t.h.

---
+++ include/dll_ws2_32.h
@@ -0,0 +1,17 @@
+#ifndef DLL_WS2_32_H
+#define DLL_WS2_32_H
+
+#include "c_types.h"
+#include "windows_t.h"
+
+#define WSASYSNOTREADY 10091
+#define WSAEINPROGRESS 10036
+
+typedef int(*WSAStartup_t)
+(
+    WORD wVersionRequired, POINTER lpWSAData
+);
+
+typedef int (*WSACleanup_t)();
+
+#endif // DLL_WS2_32_H

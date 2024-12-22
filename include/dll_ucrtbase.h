8f9d728b792f1422b052d9ec70e6285e94a56002
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-22 13:25:06 +0800

    dll_ucrtbase.h: rename ucrtbase_t.h to dll_ucrtbase.h, update #ifndef DLL_UCRTBASE_H.

---
+++ include/dll_ucrtbase.h
@@ -0,0 +1,32 @@
+#ifndef DLL_UCRTBASE_H
+#define DLL_UCRTBASE_H
+
+#include "c_types.h"
+#include "windows_t.h"
+
+typedef void* (__cdecl *ucrtbase_malloc_t)
+(
+	uint size
+);
+
+typedef void* (__cdecl *ucrtbase_calloc_t)
+(
+	uint num, uint size
+);
+
+typedef void* (__cdecl *ucrtbase_realloc_t)
+(
+	void* ptr, uint size
+);
+
+typedef void (__cdecl *ucrtbase_free_t)
+(
+	void* ptr
+);
+
+typedef uint (__cdecl *ucrtbase_msize_t)
+(
+	void* ptr
+);
+
+#endif // DLL_UCRTBASE_H

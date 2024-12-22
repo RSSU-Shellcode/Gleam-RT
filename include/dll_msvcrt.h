cc046106f8ad13495f54b54e44e1a11bbae71dd3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-22 13:24:13 +0800

    dll_msvcrt.h: rename msvcrt_t.h to dll_msvcrt.h, update #ifndef DLL_MSVCRT_H.

---
+++ include/dll_msvcrt.h
@@ -0,0 +1,32 @@
+#ifndef DLL_MSVCRT_H
+#define DLL_MSVCRT_H
+
+#include "c_types.h"
+#include "windows_t.h"
+
+typedef void* (__cdecl *msvcrt_malloc_t)
+(
+	uint size
+);
+
+typedef void* (__cdecl *msvcrt_calloc_t)
+(
+	uint num, uint size
+);
+
+typedef void* (__cdecl *msvcrt_realloc_t)
+(
+	void* ptr, uint size
+);
+
+typedef void (__cdecl *msvcrt_free_t)
+(
+	void* ptr
+);
+
+typedef uint (__cdecl *msvcrt_msize_t)
+(
+	void* ptr
+);
+
+#endif // DLL_MSVCRT_H

ff9cb82cb539013eca6be51dc65d07a49b7c0a3c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-22 13:57:53 +0800

    win_types.h: add header file that contain the Windows base type.

---
+++ include/win_types.h
@@ -0,0 +1,33 @@
+#ifndef WIN_TYPES_H
+#define WIN_TYPES_H
+
+#include "c_types.h"
+
+typedef uint8  BYTE;
+typedef uint16 WORD;
+typedef uint32 DWORD;
+typedef uint64 QWORD;
+
+typedef int8  CHAR;
+typedef int16 SHORT;
+typedef int32 LONG;
+typedef int64 LONGLONG;
+
+typedef uint UINT;
+typedef bool BOOL;
+typedef uint SIZE_T;
+
+typedef void* POINTER;
+typedef void* PVOID;
+typedef void* HANDLE;
+
+typedef void*   LPVOID;
+typedef uint8*  LPSTR;
+typedef uint16* LPWSTR;
+typedef HANDLE* LPHANDLE;
+
+typedef const void*   LPCVOID;
+typedef const uint8*  LPCSTR;
+typedef const uint16* LPCWSTR;
+
+#endif // WIN_TYPES_H

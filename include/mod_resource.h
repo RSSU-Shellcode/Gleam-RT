1dcf06f2cdddcbaa47157de3e5bea4945793f178
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-12-28 15:18:13 +0800

    mod_resource.h: add LockMutex and FreeAll, add IAT hooks about CreateMutex and CreateEvent.

--- include/mod_resource.h
+++ include/mod_resource.h
@@ -8,13 +8,21 @@
 #include "context.h"
 #include "errno.h"
 
+typedef bool (*ResLockMutex_t)(HANDLE hMutex);
+typedef bool (*ResUnlockMutex_t)(HANDLE hMutex);
+
 typedef bool  (*ResLock_t)();
 typedef bool  (*ResUnlock_t)();
 typedef errno (*ResEncrypt_t)();
 typedef errno (*ResDecrypt_t)();
+typedef errno (*ResFreeAll_t)();
 typedef errno (*ResClean_t)();
 
 typedef struct {
+    CreateMutexA_t     CreateMutexA;
+    CreateMutexW_t     CreateMutexW;
+    CreateEventA_t     CreateEventA;
+    CreateEventW_t     CreateEventW;
     CreateFileA_t      CreateFileA;
     CreateFileW_t      CreateFileW;
     FindFirstFileA_t   FindFirstFileA;
@@ -27,10 +35,14 @@ typedef struct {
     WSAStartup_t WSAStartup;
     WSACleanup_t WSACleanup;
 
+    ResLockMutex_t   LockMutex;
+    ResUnlockMutex_t UnlockMutex;
+
     ResLock_t    Lock;
     ResUnlock_t  Unlock;
     ResEncrypt_t Encrypt;
     ResDecrypt_t Decrypt;
+    ResFreeAll_t FreeAll;
     ResClean_t   Clean;
 } ResourceTracker_M;
 

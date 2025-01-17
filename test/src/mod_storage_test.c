e78882bfff094ec3786f2954a072f44f6ad9a4a0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-01-17 15:10:32 +0800

    src/mod_storage_test.c: update TestStorage_SetValue, finish GetValue and GetPointer.

--- test/src/mod_storage_test.c
+++ test/src/mod_storage_test.c
@@ -49,16 +49,101 @@ static bool TestStorage_SetValue()
         printf_s("failed to set value with id 0\n");
         return false;
     }
+
+    printf_s("set value with the different id\n");
+    id  = 16;
+    val = 1234;
+    if (!runtime->Storage.SetValue(id, &val, sizeof(val)))
+    {
+        printf_s("failed to set value with id 16\n");
+        return false;
+    }
     return true;
 }
 
 static bool TestStorage_GetValue()
 {
+    int id = 0;
+    uint val;
+    uint size;
+    if (!runtime->Storage.GetValue(id, &val, &size))
+    {
+        printf_s("failed to get value with id 0\n");
+        return false;
+    }
+    if (val != 5678)
+    {
+        printf_s("get incorrect value with id 0\n");
+        return false;
+    }
+    if (size != sizeof(val))
+    {
+        printf_s("get value with incorrect size\n");
+        return false;
+    }
+
+    printf_s("get value but not receive size\n");
+    id   = 16;
+    val  = 0;
+    size = 0;
+    if (!runtime->Storage.GetValue(id, &val, NULL))
+    {
+        printf_s("failed to get value with id 0\n");
+        return false;
+    }
+    if (val != 1234)
+    {
+        printf_s("get incorrect value with id 16\n");
+        return false;
+    }
+    if (size != 0)
+    {
+        printf_s("get value with incorrect size\n");
+        return false;
+    }
     return true;
 }
 
 static bool TestStorage_GetPointer()
 {
+    int id = 0;
+    uint* val = NULL;
+    uint  size;
+    if (!runtime->Storage.GetPointer(id, &val, &size))
+    {
+        printf_s("failed to get ponter with id 0\n");
+        return false;
+    }
+    if (*val != 5678)
+    {
+        printf_s("get incorrect value with id 0\n");
+        return false;
+    }
+    if (size != sizeof(*val))
+    {
+        printf_s("get ponter with incorrect size\n");
+        return false;
+    }
+
+    printf_s("get ponter but not receive size\n");
+    id   = 16;
+    val  = NULL;
+    size = 0;
+    if (!runtime->Storage.GetPointer(id, &val, NULL))
+    {
+        printf_s("failed to get ponter with id 0\n");
+        return false;
+    }
+    if (*val != 1234)
+    {
+        printf_s("get incorrect value with id 16\n");
+        return false;
+    }
+    if (size != 0)
+    {
+        printf_s("get ponter with incorrect size\n");
+        return false;
+    }
     return true;
 }
 

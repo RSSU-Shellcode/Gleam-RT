1120572495e61221702ff0461e3473740befe488
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-01-18 20:55:47 +0800

    args_stub.asm: update Argument_Stub data that add argument ID.

--- asm/args_stub.asm
+++ asm/args_stub.asm
@@ -2,11 +2,11 @@ IFDEF _WIN32
 .model flat
 ENDIF
 
-; +---------+----------+-----------+----------+-----------+-----------+
-; |   key   | num args | args size | checksum | arg1 size | arg1 data |
-; +---------+----------+-----------+----------+-----------+-----------+
-; | 32 byte |  uint32  |  uint32   |  uint32  |  uint32   |    var    |
-; +---------+----------+-----------+----------+-----------+-----------+
+; +---------+----------+-----------+----------+--------+----------+----------+
+; |   key   | num args | args size | checksum | arg id | arg size | arg data |
+; +---------+----------+-----------+----------+--------+----------+----------+
+; | 32 byte |  uint32  |  uint32   |  uint32  | uint32 |  uint32  |   var    |
+; +---------+----------+-----------+----------+--------+----------+----------+
 
 .code
 
@@ -15,26 +15,28 @@ IFDEF _WIN32
 ELSE
   Argument_Stub proc
 ENDIF
-
-  db 0E7h, 09Fh, 03Eh, 006h    ; 32 bytes decrypt key
-  db 0F6h, 0A5h, 08Bh, 06Eh
-  db 020h, 022h, 0F4h, 03Eh
-  db 0E9h, 0B5h, 00Bh, 08Ah
-  db 0E6h, 0ABh, 016h, 035h
-  db 010h, 0BBh, 038h, 0A6h
-  db 0CDh, 0A8h, 0B6h, 0C1h
-  db 0CDh, 00Ah, 0B0h, 015h
-  db 003h, 000h, 000h, 000h    ; record the number of the arguments
-  db 01Ch, 000h, 000h, 000h    ; record the total argument data size
-  db 073h, 0A7h, 036h, 0A4h    ; 4 bytes checksum for check header
-  db 061h, 00Bh, 00Bh, 003h    ; record the size of the argument-1
-  db 03Dh, 0FFh, 0F9h, 013h    ; argument-1 data
-  db 035h, 040h, 029h, 00Bh    ; record the size of the argument-2
-  db 016h, 029h, 047h, 04Ah    ; argument-2 data
-  db 08Fh, 0B4h, 053h, 001h
-  db 07Fh, 039h, 0F1h, 024h
-  db 0FAh, 009h, 017h, 00Eh    ; record the size of the argument-3
-                               ; argument-3 data (empty)
+  db 0F0h, 0B6h, 099h, 0F6h   ; 32 bytes decrypt key
+  db 086h, 080h, 0C4h, 0B4h
+  db 068h, 050h, 04Bh, 0CBh
+  db 0EBh, 060h, 0BAh, 01Eh
+  db 04Ah, 0D7h, 062h, 0B3h
+  db 009h, 0DAh, 0A9h, 0F9h
+  db 015h, 045h, 0DBh, 07Ah
+  db 010h, 047h, 021h, 00Dh
+  db 003h, 000h, 000h, 000h   ; record the number of the arguments
+  db 028h, 000h, 000h, 000h   ; record the total argument data size
+  db 0D0h, 013h, 081h, 094h   ; 4 bytes checksum for check header
+  db 030h, 0CFh, 025h, 03Ch   ; record the ID of the argument-1
+  db 058h, 0EAh, 029h, 0C3h   ; record the size of the argument-1
+  db 096h, 062h, 09Ah, 071h   ; argument-1 data
+  db 0BCh, 02Eh, 0D2h, 031h   ; record the ID of the argument-2
+  db 089h, 098h, 002h, 019h   ; record the size of the argument-2
+  db 081h, 059h, 0C5h, 0B4h   ; argument-2 data
+  db 05Fh, 058h, 07Ch, 053h
+  db 0BBh, 056h, 07Dh, 0C9h
+  db 032h, 0A3h, 005h, 050h   ; record the ID of the argument-3
+  db 08Fh, 0E2h, 0B9h, 047h   ; record the size of the argument-3
+                              ; argument-3 data (empty)
 IFDEF _WIN32
   _Argument_Stub@0 endp
 ELSE

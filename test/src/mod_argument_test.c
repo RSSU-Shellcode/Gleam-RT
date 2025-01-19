3afbd5ad19d0a7f5396056cd0f2efea4824d306c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-01-19 15:59:49 +0800

    src/mod_argument_test.c: update tests about get and erase argument.

--- test/src/mod_argument_test.c
+++ test/src/mod_argument_test.c
@@ -33,11 +33,12 @@ bool TestRuntime_Argument()
 static bool TestArgument_GetValue()
 {
     // get argument 0 value with size
+    uint32 id   = 0;  
     uint32 arg0 = 0;
     uint32 size = 0;
-    if (!runtime->Argument.GetValue(0, &arg0, &size))
+    if (!runtime->Argument.GetValue(id, &arg0, &size))
     {
-        printf_s("failed to get argument 0\n");
+        printf_s("failed to get argument with id 0\n");
         return false;
     }
     if (arg0 != 0x12345678)
@@ -53,10 +54,11 @@ static bool TestArgument_GetValue()
     printf_s("arg0: 0x%X, size: %d\n", arg0, size);
 
     // get argument 1 value with size
+    id = 1;
     byte arg1[12+1];
-    if (!runtime->Argument.GetValue(1, &arg1, &size))
+    if (!runtime->Argument.GetValue(id, &arg1, &size))
     {
-        printf_s("failed to get argument 1\n");
+        printf_s("failed to get argument with id 1\n");
         return false;
     }
     arg1[12] = 0x00; // set string end
@@ -73,10 +75,11 @@ static bool TestArgument_GetValue()
     printf_s("arg1: %s, size: %d\n", arg1, size);
 
     // get argument 2 value with size
+    id = 2;
     byte arg2 = 123;
-    if (!runtime->Argument.GetValue(2, &arg2, &size))
+    if (!runtime->Argument.GetValue(id, &arg2, &size))
     {
-        printf_s("failed to get argument 2\n");
+        printf_s("failed to get argument with id 2\n");
         return false;
     }
     if (arg2 != 123)
@@ -91,10 +94,11 @@ static bool TestArgument_GetValue()
     }
 
     // not receive argument size
+    id = 0;
     arg0 = 0;
-    if (!runtime->Argument.GetValue(0, &arg0, NULL))
+    if (!runtime->Argument.GetValue(id, &arg0, NULL))
     {
-        printf_s("failed to get argument 0\n");
+        printf_s("failed to get argument with id 0\n");
         return false;
     }
     if (arg0 != 0x12345678)
@@ -104,10 +108,11 @@ static bool TestArgument_GetValue()
     }
     printf_s("arg0: 0x%X\n", arg0);
 
-    // invalid index
-    if (runtime->Argument.GetValue(3, &arg0, NULL))
+    // get argument with invalid id
+    id = 3;
+    if (runtime->Argument.GetValue(id, &arg0, NULL))
     {
-        printf_s("get argument with invalid index\n");
+        printf_s("get argument with invalid id\n");
         return false;
     }
     return true;
@@ -116,11 +121,12 @@ static bool TestArgument_GetValue()
 static bool TestArgument_GetPointer()
 {
     // get argument 0 pointer with size
+    uint32  id   = 0;
     uint32* arg0 = NULL;
     uint32  size = 0;
-    if (!runtime->Argument.GetPointer(0, &arg0, &size))
+    if (!runtime->Argument.GetPointer(id, &arg0, &size))
     {
-        printf_s("failed to get argument 0\n");
+        printf_s("failed to get argument with id 0\n");
         return false;
     }
     if (*arg0 != 0x12345678)
@@ -136,10 +142,11 @@ static bool TestArgument_GetPointer()
     printf_s("arg0: 0x%X, size: %d\n", *arg0, size);
 
     // get argument 1 pointer with size
+    id = 1;
     byte* arg1 = NULL;
-    if (!runtime->Argument.GetPointer(1, &arg1, &size))
+    if (!runtime->Argument.GetPointer(id, &arg1, &size))
     {
-        printf_s("failed to get argument 1\n");
+        printf_s("failed to get argument with id 1\n");
         return false;
     }
     if (strcmp_a(arg1, "aaaabbbbccc") != 0)
@@ -155,10 +162,11 @@ static bool TestArgument_GetPointer()
     printf_s("arg1: %s, size: %d\n", arg1, size);
 
     // get argument 2 pointer with size
+    id = 2;
     byte* arg2 = (byte*)123;
-    if (!runtime->Argument.GetPointer(2, &arg2, &size))
+    if (!runtime->Argument.GetPointer(id, &arg2, &size))
     {
-        printf_s("failed to get argument 2\n");
+        printf_s("failed to get argument with id 2\n");
         return false;
     }
     if (arg2 != NULL)
@@ -173,10 +181,11 @@ static bool TestArgument_GetPointer()
     }
 
     // not receive argument size
+    id = 0;
     arg0 = NULL;
-    if (!runtime->Argument.GetPointer(0, &arg0, NULL))
+    if (!runtime->Argument.GetPointer(id, &arg0, NULL))
     {
-        printf_s("failed to get argument 0\n");
+        printf_s("failed to get argument with id 0\n");
         return false;
     }
     if (*arg0 != 0x12345678)
@@ -186,10 +195,11 @@ static bool TestArgument_GetPointer()
     }
     printf_s("arg0: 0x%X\n", *arg0);
 
-    // invalid index
-    if (runtime->Argument.GetPointer(3, &arg0, NULL))
+    // get argument with invalid id
+    id = 3;
+    if (runtime->Argument.GetPointer(id, &arg0, NULL))
     {
-        printf_s("get argument with invalid index\n");
+        printf_s("get argument with invalid id\n");
         return false;
     }
     return true;
@@ -197,37 +207,29 @@ static bool TestArgument_GetPointer()
 
 static bool TestArgument_Erase()
 {
-    if (!runtime->Argument.Erase(1))
+    uint32 id = 1;
+    if (!runtime->Argument.Erase(id))
     {
-        printf_s("failed to erase argument 1\n");
+        printf_s("failed to erase argument with id 1\n");
         return false;
     }
     printf_s("erase argument 1\n");
 
     byte*  arg1 = NULL;
     uint32 size = 0;
-    if (!runtime->Argument.GetPointer(1, &arg1, &size))
+    if (runtime->Argument.GetPointer(id, &arg1, &size))
     {
-        printf_s("failed to get argument 1\n");
-        return false;
-    }
-    if (strcmp_a(arg1, "aaaabbbbccc") == 0)
-    {
-        printf_s("argument 1 is not be erased\n");
-        return false;
-    }
-    if (size != 12)
-    {
-        printf_s("argument 1 size is incorrect\n");
+        printf_s("get argument with id 1\n");
         return false;
     }
     printf_s("check erased argument 1\n");
 
     // check arguments around it
+    id = 0;
     uint32* arg0 = NULL;
-    if (!runtime->Argument.GetPointer(0, &arg0, &size))
+    if (!runtime->Argument.GetPointer(id, &arg0, &size))
     {
-        printf_s("failed to get argument 0\n");
+        printf_s("failed to get argument with id 0\n");
         return false;
     }
     if (*arg0 != 0x12345678)
@@ -241,10 +243,11 @@ static bool TestArgument_Erase()
         return false;
     }
 
+    id = 2;
     byte* arg2 = (byte*)123;
-    if (!runtime->Argument.GetPointer(2, &arg2, &size))
+    if (!runtime->Argument.GetPointer(id, &arg2, &size))
     {
-        printf_s("failed to get argument 2\n");
+        printf_s("failed to get argument with id 2\n");
         return false;
     }
     if (arg2 != NULL)
@@ -265,23 +268,27 @@ static bool TestArgument_EraseAll()
     runtime->Argument.EraseAll();
     printf_s("erase all arguments\n");
 
+    uint32  id   = 0;
     uint32* arg0 = NULL;
     uint32  size = 0;
-    if (!runtime->Argument.GetPointer(0, &arg0, &size))
+    if (runtime->Argument.GetPointer(id, &arg0, &size))
     {
-        printf_s("failed to get argument 0\n");
+        printf_s("get argument with id 0\n");
         return false;
     }
-    if (*arg0 == 0x12345678)
+
+    id = 1;
+    if (runtime->Argument.GetPointer(id, &arg0, &size))
     {
-        printf_s("argument 0 is not be erased\n");
+        printf_s("get argument with id 1\n");
         return false;
     }
-    if (size == 4)
+
+    id = 2;
+    if (runtime->Argument.GetPointer(id, &arg0, &size))
     {
-        printf_s("argument 0 size is not be erased\n");
+        printf_s("get argument with id 2\n");
         return false;
     }
-    printf_s("check erased argument 0\n");
     return true;
 }

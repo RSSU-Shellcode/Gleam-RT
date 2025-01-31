fc3785002aab67b29ab084c2b8d5c87b1cf6e3db
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-01-31 17:03:40 +0800

    win_crypto_test.c: add sub test about 16 bytes to TestWinCrypto_AESDecrypt.

--- test/src/win_crypto_test.c
+++ test/src/win_crypto_test.c
@@ -75,7 +75,7 @@ static bool TestWinCrypto_SHA1()
         0xAD, 0xE3, 0x33, 0x31, 0x47, 0x20, 0x2C, 0x3B,
         0x44, 0x3E, 0x37, 0x6F,
     };
-    if (!mem_equal(hash, expected, 20))
+    if (!mem_equal(expected, hash, 20))
     {
         printf_s("get incorrect SHA1 hash\n");
         return false;
@@ -118,7 +118,7 @@ static bool TestWinCrypto_AESEncrypt()
 
 static bool TestWinCrypto_AESDecrypt()
 {
-    byte data[4] = { 1, 2, 3, 4 };
+    byte data1[4] = { 1, 2, 3, 4 };
     byte key[32] = {
         0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x03, 
         0x00, 0x01, 0x02, 0x03, 0x00, 0x01, 0x02, 0x03, 
@@ -127,13 +127,17 @@ static bool TestWinCrypto_AESDecrypt()
     };
 
     byte* cipher; uint cipherLen;
-    errno err = runtime->WinCrypto.AESEncrypt(data, sizeof(data), key, &cipher, &cipherLen);
+    errno err = runtime->WinCrypto.AESEncrypt(data1, sizeof(data1), key, &cipher, &cipherLen);
     if (err != NO_ERROR)
     {
         printf_s("failed to encrypt data: 0x%X\n", err);
         return false;
     }
-
+    if (cipherLen != 32)
+    {
+        printf_s("invalid cipher data length\n");
+        return false;
+    }
     byte* plain; uint plainLen;
     err = runtime->WinCrypto.AESDecrypt(cipher, cipherLen, key, &plain, &plainLen);
     if (err != NO_ERROR)
@@ -148,10 +152,61 @@ static bool TestWinCrypto_AESDecrypt()
         printf_s("invalid plain data length\n");
         return false;
     }
-    byte expected[4] = { 0x01, 0x02, 0x03, 0x04 };
-    if (!mem_equal(plain, expected, 4))
+    byte expected1[4] = { 1, 2, 3, 4 };
+    if (!mem_equal(expected1, plain, sizeof(expected1)))
     {
-        printf_s("get incorrect SHA1 hash\n");
+        printf_s("get incorrect plain data\n");
+        return false;
+    }
+    if (!mem_equal(expected1, data1, sizeof(expected1)))
+    {
+        printf_s("original data is changed\n");
+        return false;
+    }
+
+    runtime->Memory.Free(cipher);
+    runtime->Memory.Free(plain);
+
+    byte data2[16] = { 
+        1, 2, 3, 4, 5, 6, 7, 8, 
+        1, 2, 3, 4, 5, 6, 7, 8,
+    };
+    err = runtime->WinCrypto.AESEncrypt(data2, sizeof(data2), key, &cipher, &cipherLen);
+    if (err != NO_ERROR)
+    {
+        printf_s("failed to encrypt data: 0x%X\n", err);
+        return false;
+    }
+    if (cipherLen != 48)
+    {
+        printf_s("invalid cipher data length\n");
+        return false;
+    }
+    err = runtime->WinCrypto.AESDecrypt(cipher, cipherLen, key, &plain, &plainLen);
+    if (err != NO_ERROR)
+    {
+        printf_s("failed to decrypt data: 0x%X\n", err);
+        return false;
+    }
+
+    printHexBytes(plain, plainLen);
+    if (plainLen != 16)
+    {
+        printf_s("invalid plain data length\n");
+        return false;
+    }
+    byte expected2[16] = { 
+        1, 2, 3, 4, 5, 6, 7, 8, 
+        1, 2, 3, 4, 5, 6, 7, 8,
+    };
+    if (!mem_equal(expected2, plain, sizeof(expected2)))
+    {
+        printf_s("get incorrect plain data\n");
+        return false;
+    }
+    if (!mem_equal(expected2, data2, sizeof(expected2)))
+    {
+        printf_s("original data is changed\n");
         return false;
     }
 

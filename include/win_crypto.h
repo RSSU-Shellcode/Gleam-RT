9e8beb0bbd21b053867d4739a72333e9f21611ff
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-02-01 19:06:48 +0800

    win_crypto.h: add typedef WCGenRSAKey_t, add GenRSAKey to WinCrypto_M.

--- include/win_crypto.h
+++ include/win_crypto.h
@@ -7,7 +7,7 @@
 
 // The allocated buffer must call Runtime_M.Memory.Free().
 // 
-// The AES is use CBC mode with 256 bit key and PKCS5.
+// The AES is use CBC mode with 256 bit key and PKCS#5.
 // 
 // +---------+-------------+
 // |   IV    | cipher data |
@@ -17,11 +17,15 @@
 //
 // The RSA private key length is 4096 bit.
 
+#define WC_SHA1_SIZE    20
 #define WC_AES_KEY_SIZE 32
 #define WC_AES_IV_SIZE  16
-#define WC_RSA_KEY_SIZE 512
+
+#define WC_RSA_KEY_USAGE_SIGN 1
+#define WC_RSA_KEY_USAGE_KEYX 2
 
 typedef errno (*WCRandBuffer_t)(byte* data, uint len);
+typedef errno (*WCGenRSAKey_t)(uint bits, byte** data, uint* len, uint usage);
 typedef errno (*WCSHA1_t)(byte* data, uint len, byte* hash);
 typedef errno (*WCAESEncrypt_t)(byte* data, uint len, byte* key, byte** out, uint* outLen);
 typedef errno (*WCAESDecrypt_t)(byte* data, uint len, byte* key, byte** out, uint* outLen);
@@ -32,6 +36,7 @@ typedef errno (*WCUninstall_t)();
 
 typedef struct {
     WCRandBuffer_t RandBuffer;
+    WCGenRSAKey_t  GenRSAKey;
     WCSHA1_t       SHA1;
     WCAESEncrypt_t AESEncrypt;
     WCAESDecrypt_t AESDecrypt;

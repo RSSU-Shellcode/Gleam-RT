7e5ac85b266c5595ead7b67ccba61adc9716e78f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-02-20 18:05:14 +0800

    win_http.h: add more fields about timeout to HTTP_Request.

--- include/win_http.h
+++ include/win_http.h
@@ -13,20 +13,21 @@
 typedef struct {
     UTF16 URL; // https://www.example.com/test.txt
 
-    UTF16  Headers;     // split by "\r\n"
-    UTF16  ContentType; // for POST method
-    UTF16  UserAgent;   // default User-Agent
-    UTF16  ProxyURL;    // http://user:pass@host.com/
-    uint   MaxBodySize; // default is no limit
-    uint32 Timeout;     // millseconds
-    uint8  AccessType;  // reference document about WinHttpOpen
+    UTF16 Headers;        // split by "\r\n"
+    UTF16 UserAgent;      // default User-Agent
+    UTF16 ProxyURL;       // http://user:pass@host.com/
+    uint  MaxBodySize;    // zero is no limit
+    uint  ConnectTimeout; // millseconds
+    uint  SendTimeout;    // millseconds
+    uint  ReceiveTimeout; // millseconds
+    uint8 AccessType;     // reference document about WinHttpOpen
 
     databuf* Body;
 } HTTP_Request;
 
 typedef struct {
-    int32 StatusCode;
-    UTF16 Headers;
+    int32 StatusCode; // example 200, 404
+    UTF16 Headers;    // split by "\r\n"
 
     databuf Body;
 } HTTP_Response;

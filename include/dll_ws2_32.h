e41fbf8f7b40067c14782146f8e36627944209ca
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-01 15:32:41 +0800

    dll_ws2_32.h: add more typedef functions about create socket.

--- include/dll_ws2_32.h
+++ include/dll_ws2_32.h
@@ -7,11 +7,40 @@
 #define WSASYSNOTREADY 10091
 #define WSAEINPROGRESS 10036
 
-typedef int(*WSAStartup_t)
+typedef HANDLE SOCKET;
+
+typedef int (*WSAStartup_t)
 (
     WORD wVersionRequired, POINTER lpWSAData
 );
 
 typedef int (*WSACleanup_t)();
+
+typedef SOCKET (*WSASocketA_t)
+(
+    int af, int type, int protocol, POINTER lpProtocolInfo, 
+    POINTER g, DWORD dwFlags
+);
+
+typedef SOCKET (*WSASocketW_t)
+(
+    int af, int type, int protocol, POINTER lpProtocolInfo, 
+    POINTER g, DWORD dwFlags
+);
+
+typedef SOCKET (*socket_t)
+(
+    int af, int type, int protocol
+);
+
+typedef SOCKET (*accept_t)
+(
+    SOCKET s, POINTER addr, int* addrlen
+);
+
+typedef int (*closesocket_t)
+(
+    SOCKET s
+);
 
 #endif // DLL_WS2_32_H

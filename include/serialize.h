022764827ff2d7204ecdac5045d0df2c994245d7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-10 22:13:23 +0800

    serialize.h: add header file about serialization, add comment.

---
+++ include/serialize.h
@@ -0,0 +1,34 @@
+#ifndef SERIALIZE_H
+#define SERIALIZE_H
+
+#include "c_types.h"
+
+// serialized data structure
+// +---------+----------+----------+----------+------------+
+// |  magic  |  item 1  |  item 2  |  item e  |  raw data  |
+// +---------+----------+----------+----------+------------+
+// |  uint32 |  uint32  |  uint32  |  uint32  |    var     |
+// +---------+----------+----------+----------+------------+
+//
+// item data structure
+// 0，，，，，，， value or pointer
+// ，0000000 data length
+
+#define SERIALIZE_HEADER_MAGIC 0xFFFFFFFF
+#define SERIALIZE_ITEM_END     0x00000000
+
+#define SERIALIZE_FLAG_VALUE   0x00000000
+#define SERIALIZE_FLAG_POINTER 0x80000000
+
+#define SERIALIZE_MASK_FLAG   0x80000000
+#define SERIALIZE_MASK_LENGTH 0x7FFFFFFF
+
+// Serialize is used to serialize structure to a buffer.
+// If success, return the serialized data length. If failed, return 0.
+// If serialized is NULL, it will calculate the serialized data length.
+uint32 Serialize(uint32* descriptor, void* data, void* serialized);
+
+// Unserialize is used to unserialize data to a structure.
+bool Unserialize(void* serialized, void* data);
+
+#endif // SERIALIZE_H

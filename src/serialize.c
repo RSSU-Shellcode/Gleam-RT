ae31fbec4869b5f0ae20979e1b9408e523d2fb02
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-10 23:10:16 +0800

    serialize.c: implement Unserialize, finish unserialize data to structure field.

--- src/serialize.c
+++ src/serialize.c
@@ -68,5 +68,56 @@ uint32 Serialize(uint32* descriptor, void* data, void* serialized)
 
 bool Unserialize(void* serialized, void* data)
 {
-
+    byte* buffer  = serialized;
+    byte* dataptr = data;
+    // check is valid serialized data
+    if (*(uint32*)buffer != SERIALIZE_HEADER_MAGIC)
+    {
+        return false;
+    }
+    buffer += sizeof(uint32);
+    // calculate the number of the fields
+    uint numFields = 0;
+    uint32* desc_p = (uint32*)buffer;
+    for (;;)
+    {
+        uint32 desc = *desc_p;
+        if (desc == SERIALIZE_ITEM_END)
+        {
+            break;
+        }
+        numFields++;
+        desc_p++;
+    }
+    byte* datasrc = buffer + (numFields + 1) * 4;
+    // unserialize data to structure field
+    desc_p = (uint32*)buffer;
+    for (;;)
+    {
+        uint32 desc = *desc_p;
+        if (desc == SERIALIZE_ITEM_END)
+        {
+            break;
+        }
+        uint32 size = desc & SERIALIZE_MASK_LENGTH;
+        switch (desc & SERIALIZE_MASK_FLAG)
+        {
+        case SERIALIZE_FLAG_VALUE:
+            mem_copy(dataptr, datasrc, size);
+            dataptr += size;
+            break;
+        case SERIALIZE_FLAG_POINTER:
+            uintptr ptr = (uintptr)datasrc;
+            if (size == 0)
+            {
+                ptr = 0; // set NULL pointer
+            }
+            mem_copy(dataptr, &ptr, sizeof(uintptr));
+            dataptr += sizeof(uintptr);
+            break;
+        }
+        datasrc += size;
+        desc_p++;
+    }
+    return true;
 }

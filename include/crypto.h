ba2374781d56c67f299dc65524450a1b5000bce1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-15 14:45:00 +0800

    crypto.h: replace argument type from byte* buf to void* buf.

--- include/crypto.h
+++ include/crypto.h
@@ -7,10 +7,10 @@
 #define CRYPTO_KEY_SIZE 32
 
 // EncryptBuf is used to encrypt data in buffer with 256 bit key.
-void EncryptBuf(byte* buf, uint size, byte* key, byte* iv);
+void EncryptBuf(void* buf, uint size, byte* key, byte* iv);
 
 // DecryptBuf is used to decrypt data in buffer with 256 bit key.
-void DecryptBuf(byte* buf, uint size, byte* key, byte* iv);
+void DecryptBuf(void* buf, uint size, byte* key, byte* iv);
 
 // XORBuf is used to xor data in buffer with key.
 void XORBuf(void* buf, uint bufSize, void* key, uint keySize);

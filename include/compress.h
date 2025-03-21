0285de6d37424b4a419e18ae84740cdd123e8ccf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-21 13:59:57 +0800

    compress.h: add comment about If dst is NULL to functions.

--- include/compress.h
+++ include/compress.h
@@ -5,9 +5,11 @@
 
 // Compress is used to compress data with LZSS.
 // If return value is -1, window size is invalid.
+// If dst is NULL, calculate the compressed length.
 uint Compress(void* dst, void* src, uint len, uint window);
 
 // Decompress is used to decompress data with LZSS.
+// If dst is NULL, calculate the raw data length.
 uint Decompress(void* dst, void* src, uint len);
 
 #endif // COMPRESS_H

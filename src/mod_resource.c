6982180e65ff76271e6b0b5dc0d1d9a247f79ff6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-03 15:30:58 +0800

    mod_resource.c: improve RT_FreeAllMu about process SetLastErrno.

--- src/mod_resource.c
+++ src/mod_resource.c
@@ -1923,8 +1923,12 @@ bool RT_FreeAllMu()
         return false;
     }
 
-    SetLastErrno(errno);
-    return errno == NO_ERROR;
+    if (errno != NO_ERROR)
+    {
+        SetLastErrno(errno);
+        return false;
+    }
+    return true;
 }
 
 __declspec(noinline)

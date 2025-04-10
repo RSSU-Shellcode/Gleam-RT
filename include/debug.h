255119a451a69ce6b45cbe68dc64a02c4d6f355b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-10 17:36:16 +0800

    debug.h: remove kernel object name about event, move the arch to prefix.

--- include/debug.h
+++ include/debug.h
@@ -6,29 +6,17 @@
 
 #ifdef RELEASE_MODE
     #define NAME_RT_MUTEX_GLOBAL  NULL
-    #define NAME_RT_MUTEX_SLEEP   NULL
-    #define NAME_RT_EVENT_ARRIVE  NULL
-    #define NAME_RT_EVENT_DONE    NULL
-    #define NAME_RT_MUTEX_EVENT   NULL
     #define NAME_RT_TIMER_SLEEPHR NULL
     #define NAME_TT_TIMER_SLEEP   NULL
 #else
 #ifdef _WIN64
-    #define NAME_RT_MUTEX_GLOBAL  "RT_Core_Global_x64"
-    #define NAME_RT_MUTEX_SLEEP   "RT_Core_Sleep_x64"
-    #define NAME_RT_EVENT_ARRIVE  "RT_Core_Arrive_x64"
-    #define NAME_RT_EVENT_DONE    "RT_Core_Done_x64"
-    #define NAME_RT_MUTEX_EVENT   "RT_Core_Event_x64"
-    #define NAME_RT_TIMER_SLEEPHR L"RT_Method_SleepHR_x64"
-    #define NAME_TT_TIMER_SLEEP   "RT_TT_Sleep_x64"
+    #define NAME_RT_MUTEX_GLOBAL  "x64_RT_Core_Global"
+    #define NAME_RT_TIMER_SLEEPHR "x64_RT_Core_SleepHR"
+    #define NAME_TT_TIMER_SLEEP   "x64_RT_TT_Sleep"
 #elif _WIN32
-    #define NAME_RT_MUTEX_GLOBAL  "RT_Core_Global_x86"
-    #define NAME_RT_MUTEX_SLEEP   "RT_Core_Sleep_x86"
-    #define NAME_RT_EVENT_ARRIVE  "RT_Core_Arrive_x86"
-    #define NAME_RT_EVENT_DONE    "RT_Core_Done_x86"
-    #define NAME_RT_MUTEX_EVENT   "RT_Core_Event_x86"
-    #define NAME_RT_TIMER_SLEEPHR L"RT_Method_SleepHR_x86"
-    #define NAME_TT_TIMER_SLEEP   "RT_TT_Sleep_x86"
+    #define NAME_RT_MUTEX_GLOBAL  "x86_RT_Core_Global"
+    #define NAME_RT_TIMER_SLEEPHR "x86_RT_Core_SleepHR"
+    #define NAME_TT_TIMER_SLEEP   "x86_RT_TT_Sleep"
 #endif
 #endif // RELEASE_MODE
 
@@ -38,11 +26,11 @@
     #define NAME_LDR_MUTEX_STATUS NULL
 #else
 #ifdef _WIN64
-    #define NAME_LDR_MUTEX_GLOBAL "LDR_Global_x64"
-    #define NAME_LDR_MUTEX_STATUS "LDR_Status_x64"
+    #define NAME_LDR_MUTEX_GLOBAL "x64_LDR_Global"
+    #define NAME_LDR_MUTEX_STATUS "x64_LDR_Status"
 #elif _WIN32
-    #define NAME_LDR_MUTEX_GLOBAL "LDR_Global_x86"
-    #define NAME_LDR_MUTEX_STATUS "LDR_Status_x86"
+    #define NAME_LDR_MUTEX_GLOBAL "x86_LDR_Global"
+    #define NAME_LDR_MUTEX_STATUS "x86_LDR_Status"
 #endif
 #endif // RELEASE_MODE
 

fc3e0867cd7e40f151bf40b968403971efddc314
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-23 18:33:41 +0800

    runtime.h: add macros and typedef about memory scanner module.

--- include/runtime.h
+++ include/runtime.h
@@ -270,6 +270,24 @@ typedef uint (*Decompress_t)(void* dst, void* src, uint len);
 typedef uint32 (*Serialize_t)(uint32* descriptor, void* data, void* serialized);
 typedef bool   (*Unserialize_t)(void* serialized, void* data);
 
+// about memory scanner module
+// 
+// MemScan is used to scans data in the memory of the current process.
+// The return value is the number of results scanned.
+//
+// example:
+//   uintptr results[10];
+//   MemScan("F1 F2 ?? A1", results, arrlen(results));
+
+#ifndef MEM_SCANNER_H
+
+#define ERR_MEMSCAN_NOT_FOUND_API 0x80000001
+#define ERR_MEMSCAN_VIRTUAL_QUERY 0x80000002
+
+#endif // MEM_SCANNER_H
+
+typedef uint (*MemScan_t)(byte* pattern, uintptr* results, uint maxItem);
+
 // GetProcAddress, GetProcAddressByName and GetProcAddressByHash
 // are use Hash API module for implement original GetProcAddress.
 // GetProcAddressOriginal is not recommend, usually use

17b506280f5cb815bee8473e743426435cceba8a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-24 20:31:40 +0800

    src/mem_scanner_test.c: add printResults, adjust reuslts buffer.

--- test/src/mem_scanner_test.c
+++ test/src/mem_scanner_test.c
@@ -7,6 +7,8 @@
 
 static bool TestMemScan();
 
+static void printResults(uintptr* results, uint num);
+
 bool TestMemScanner()
 {
     test_t tests[] = 
@@ -27,9 +29,30 @@ bool TestMemScanner()
 
 static bool TestMemScan()
 {
-    uintptr results[10]; // "test"
+    uintptr results[1000]; // "test"
     uint num = MemScan("74 65 73 74", results, arrlen(results));
+    if (num == -1)
+    {
+        printf_s("failed to scan target data: 0x%X\n", GetLastErrno());
+        return false;
+    }
+    printResults(results, num);
+
+    // "74 65 73 74 "
+    // "74 65 ?? 74"
+    // "?A"
+    // "A?"
 
     printf_s("test MemScan passed\n");
     return true;
+}
+
+static void printResults(uintptr* results, uint num)
+{
+    for (uint i = 0; i < num; i++)
+    {
+        printf_s("%zu: 0x%zX\n", i, *results);
+        results++;
+    }
+    printf_s("num result: %zu\n", num);
 }

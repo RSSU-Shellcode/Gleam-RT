5e9b3937281e5489a6bb1cbcbaabbc53b548a161
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-24 20:23:52 +0800

    mem_scanner.c: implement MemScan with fast scan mode, wait arbitrary mode.

--- src/mem_scanner.c
+++ src/mem_scanner.c
@@ -3,6 +3,7 @@
 #include "lib_memory.h"
 #include "lib_match.h"
 #include "hash_api.h"
+#include "errno.h"
 #include "mem_scanner.h"
 
 #ifdef _WIN64
@@ -11,15 +12,15 @@
     #define END_ADDRESS 0xFFFFFFFF
 #endif
 
-#define COND_TYPE_EXACT_VAL 1
-#define COND_TYPE_ARBITRARY 2
+#define COND_TYPE_EXACT_VAL 0x01
+#define COND_TYPE_ARBITRARY 0x02
 
 #define PATTERN_TYPE_ARBITRARY 0xFE
 #define PATTERN_TYPE_INVALID   0xFF
 
 static uint parsePattern(byte* pattern, uint16* condition);
 static byte charToValue(byte b);
-static bool isPageReadable(DWORD protect);
+static bool isRegionReadable(DWORD protect);
 
 #pragma optimize("t", on)
 
@@ -33,7 +34,8 @@ uint MemScan(byte* pattern, uintptr* results, uint maxItem)
 #endif
     if (VirtualQuery == NULL)
     {
-        return ERR_MEMSCAN_NOT_FOUND_API;
+        SetLastErrno(ERR_MEMSCAN_NOT_FOUND_API);
+        return (uint)(-1);
     }
 
     // parse pattern to condition array
@@ -42,36 +44,91 @@ uint MemScan(byte* pattern, uintptr* results, uint maxItem)
     uint numCond = parsePattern(pattern, condition);
     if (numCond == 0)
     {
-        return ERR_MEMSCAN_INVALID_CONDITION;
+        SetLastErrno(ERR_MEMSCAN_INVALID_CONDITION);
+        return (uint)(-1);
+    }
+
+    // check can use fast mode
+    byte fastValue[32];
+    mem_init(fastValue, sizeof(fastValue));
+    bool canFast  = true;
+    uint condSize = 0;
+    for (uint i = 0; i < arrlen(condition); i++)
+    {
+        uint16 cond = condition[i];
+        if (cond == 0x0000)
+        {
+            break;
+        }
+        if ((cond >> 8) == COND_TYPE_ARBITRARY)
+        {
+            canFast = false;
+        } else {
+            fastValue[i] = cond & 0x00FF;
+        }
+        condSize++;
     }
 
     // scan memory region
     MEMORY_BASIC_INFORMATION mbi;
     mem_init(&mbi, sizeof(mbi));
-    uintptr address   = 0;
-    uint    numResult = 0;
+    uintptr address = 0;
+    uint numResults = 0;
     while (address < END_ADDRESS)
     {
         // query memory region information
         if (VirtualQuery((LPCVOID)address, &mbi, sizeof(mbi)) == 0)
         {
-            return ERR_MEMSCAN_VIRTUAL_QUERY;
+            SetLastErrno(ERR_MEMSCAN_VIRTUAL_QUERY);
+            return (uint)(-1);
         }
         if (mbi.RegionSize == 0)
         {
             break;
         }
-        // search memory region
         uint size = mbi.RegionSize - (address - (uintptr)(mbi.BaseAddress));
-        if (!isPageReadable(mbi.Protect))
+        if (mbi.State != MEM_COMMIT || !isRegionReadable(mbi.Protect))
         {
             address += size;
             continue;
         }
-
+        // scan memory region
+        uintptr addr = address;
+        for (;;)
+        {
+            if (numResults >= maxItem)
+            {
+                return numResults;
+            }
+            int64 s = (int64)(address + size) - (int64)addr;
+            if (s < (int64)condSize)
+            {
+                break;
+            }
+            if (canFast)
+            {
+                integer idx = MatchBytes((byte*)addr, s, fastValue, condSize);
+                if (idx == -1)
+                {
+                    break;
+                } else {
+                    uintptr result = addr + idx;
+                    // skip fake result from fastValue in the stack
+                    if ((byte*)result != fastValue)
+                    {
+                        *results = result;
+                        results++;
+                        numResults++;
+                    }
+                    addr = result + condSize;
+                }
+            } else {
+                addr++;
+            }
+        }
         address += size;
     }
-    return 0;
+    return numResults;
 }
 
 static uint parsePattern(byte* pattern, uint16* condition)
@@ -119,6 +176,15 @@ static uint parsePattern(byte* pattern, uint16* condition)
         case PATTERN_TYPE_INVALID:
             return 0;
         }
+        // generate the condition
+        if (arbitrary)
+        {
+            *condition = (COND_TYPE_ARBITRARY << 8) + 0;
+        } else {
+            byte exactVal = val1 * 16 + val2;
+            *condition = (COND_TYPE_EXACT_VAL << 8) + exactVal;
+        }
+        numCond++;
         // parse the third character
         pattern++;
         switch (*pattern)
@@ -129,14 +195,6 @@ static uint parsePattern(byte* pattern, uint16* condition)
             return numCond;
         default:
             return 0;
-        }
-        // generate the condition
-        if (arbitrary)
-        {
-            *condition = COND_TYPE_ARBITRARY << 8;
-        } else {
-            byte exactVal = val1 * 16 + val2;
-            *condition = (COND_TYPE_EXACT_VAL << 8) + exactVal;
         }
         // reset status
         arbitrary = false;
@@ -168,7 +226,7 @@ static byte charToValue(byte b)
     return PATTERN_TYPE_INVALID;
 }
 
-static bool isPageReadable(DWORD protect)
+static bool isRegionReadable(DWORD protect)
 {
     switch (protect)
     {

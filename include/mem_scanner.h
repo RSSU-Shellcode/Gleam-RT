571dda5d584586eade01a063013e3aa5ec91dbf9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-24 18:55:38 +0800

    mem_scanner.h: add ERR_MEMSCAN_INVALID_CONDITION about parsePattern.

--- include/mem_scanner.h
+++ include/mem_scanner.h
@@ -3,8 +3,9 @@
 
 #include "c_types.h"
 
-#define ERR_MEMSCAN_NOT_FOUND_API 0x80000001
-#define ERR_MEMSCAN_VIRTUAL_QUERY 0x80000002
+#define ERR_MEMSCAN_NOT_FOUND_API     0x80000001
+#define ERR_MEMSCAN_VIRTUAL_QUERY     0x80000002
+#define ERR_MEMSCAN_INVALID_CONDITION 0x80000003
 
 // MemScan is used to scans data in the memory of the current process.
 // The return value is the number of results scanned.

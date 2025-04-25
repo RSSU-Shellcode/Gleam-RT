b8465617677c609fc39dcf5b9ece720f306f0402
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-25 14:16:39 +0800

    mem_scanner.h: delete macros about errors, improve and move to errno.h.

--- include/mem_scanner.h
+++ include/mem_scanner.h
@@ -3,10 +3,6 @@
 
 #include "c_types.h"
 
-#define ERR_MEMSCAN_NOT_FOUND_API     0x80000001
-#define ERR_MEMSCAN_VIRTUAL_QUERY     0x80000002
-#define ERR_MEMSCAN_INVALID_CONDITION 0x80000003
-
 // MemScan is used to scans data in the memory of the current process.
 // The return value is the number of results scanned.
 uint MemScan(byte* pattern, uintptr* results, uint maxItem);

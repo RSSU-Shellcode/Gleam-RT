627c8ab4e58e72e135c234043807d0d1601dce55
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-26 14:01:09 +0800

    hash_api.bat: add Sysmon section, add CreateWaitableTimerA and SetWaitableTimer.

--- script/hash_api.bat
+++ script/hash_api.bat
@@ -216,6 +216,11 @@ hash_api -fmt 64 -conc -mod "advapi32.dll" -func CryptSignHashA
 hash_api -fmt 64 -conc -mod "advapi32.dll" -func CryptVerifySignatureA
 echo.
 
+echo [Sysmon]
+hash_api -fmt 64 -conc -func CreateWaitableTimerA
+hash_api -fmt 64 -conc -func SetWaitableTimer
+echo.
+
 echo ------------------------x86------------------------
 
 echo [Runtime Core]
@@ -425,6 +430,11 @@ hash_api -fmt 32 -conc -mod "advapi32.dll" -func CryptDecrypt
 hash_api -fmt 32 -conc -mod "advapi32.dll" -func CryptDestroyKey
 hash_api -fmt 32 -conc -mod "advapi32.dll" -func CryptSignHashA
 hash_api -fmt 32 -conc -mod "advapi32.dll" -func CryptVerifySignatureA
+echo.
+
+echo [Sysmon]
+hash_api -fmt 32 -conc -func CreateWaitableTimerA
+hash_api -fmt 32 -conc -func SetWaitableTimer
 echo.
 
 pause

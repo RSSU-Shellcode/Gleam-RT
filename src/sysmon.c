6f931f23f6ed78811bf19d8ceabf5cb8e835ee3c
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-26 14:04:02 +0800

    sysmon.c: finish InitSysmon and initSysmonAPI, add other methods.

--- src/sysmon.c
+++ src/sysmon.c
@@ -33,24 +33,136 @@ errno SM_Pause();
 errno SM_Continue();
 errno SM_Stop();
 
-// hard encoded address in getModulePointer for replacement
+// hard encoded address in getSysmonPointer for replacement
 #ifdef _WIN64
-    #define MODULE_POINTER 0x7FABCDEF111111F1
+    #define SYSMON_POINTER 0x7FABCDEF111111F1
 #elif _WIN32
-    #define MODULE_POINTER 0x7FABCDF1
+    #define SYSMON_POINTER 0x7FABCDF1
 #endif
-static Sysmon* getModulePointer();
+static Sysmon* getSysmonPointer();
+
+static bool initSysmonAPI(Sysmon* sysmon, Context* context);
+static bool updateSysmonPointer(Sysmon* sysmon);
+static bool recoverSysmonPointer(Sysmon* sysmon);
+static bool initSysmonEnvironment(Sysmon* sysmon, Context* context);
+static void eraseSysmonMethods(Context* context);
+static void cleanSysmon(Sysmon* sysmon);
 
 Sysmon_M* InitSysmon(Context* context)
 {
     // set structure address
     uintptr address = context->MainMemPage;
-    uintptr moduleAddr = address + 24000 + RandUintN(address, 128);
+    uintptr sysmonAddr = address + 24000 + RandUintN(address, 128);
     uintptr methodAddr = address + 25000 + RandUintN(address, 128);
-    // initialize module
-    Sysmon* module = (Sysmon*)moduleAddr;
-    mem_init(module, sizeof(Sysmon));
+    // initialize sysmon
+    Sysmon* sysmon = (Sysmon*)sysmonAddr;
+    mem_init(sysmon, sizeof(Sysmon));
     // store options
-    module->NotEraseInstruction = context->NotEraseInstruction;
+    sysmon->NotEraseInstruction = context->NotEraseInstruction;
     errno errno = NO_ERROR;
+    for (;;)
+    {
+        if (!initSysmonAPI(sysmon, context))
+        {
+            errno = ERR_SYSMON_INIT_API;
+            break;
+        }
+        if (!updateSysmonPointer(sysmon))
+        {
+            errno = ERR_SYSMON_UPDATE_PTR;
+            break;
+        }
+        if (!initSysmonEnvironment(sysmon, context))
+        {
+            errno = ERR_SYSMON_INIT_ENV;
+            break;
+        }
+        break;
+    }
+    eraseSysmonMethods(context);
+    if (errno != NO_ERROR)
+    {
+        cleanSysmon(sysmon);
+        SetLastErrno(errno);
+        return NULL;
+    }
+    // create methods for tracker
+    Sysmon_M* method = (Sysmon_M*)methodAddr;
+    // methods for user
+    method->GetStatus = GetFuncAddr(&SM_GetStatus);
+    // methods for runtime
+    method->Pause    = GetFuncAddr(&SM_Pause);
+    method->Continue = GetFuncAddr(&SM_Continue);
+    method->Stop     = GetFuncAddr(&SM_Stop);
+    return sysmon;
+}
+
+__declspec(noinline)
+static bool initSysmonAPI(Sysmon* sysmon, Context* context)
+{
+    typedef struct { 
+        uint hash; uint key; void* proc;
+    } winapi;
+    winapi list[] =
+#ifdef _WIN64
+    {
+        { 0xC8EB9C09DC007FB8, 0x87980B49B926FE1D }, // CreateWaitableTimerA
+        { 0x3A5329D6B69F9A72, 0x3E19B62A8A1EDA64 }, // SetWaitableTimer
+    };
+#elif _WIN32
+    {
+        { 0xA508BB38, 0x7323A00D }, // CreateWaitableTimerA
+        { 0x10F559AB, 0xA7FD156A }, // SetWaitableTimer
+    };
+#endif
+    for (int i = 0; i < arrlen(list); i++)
+    {
+        void* proc = FindAPI(list[i].hash, list[i].key);
+        if (proc == NULL)
+        {
+            return false;
+        }
+        list[i].proc = proc;
+    }
+    sysmon->CreateWaitableTimerA = list[0x00].proc;
+    sysmon->SetWaitableTimer     = list[0x01].proc;
+
+    sysmon->ReleaseMutex        = context->ReleaseMutex;
+    sysmon->WaitForSingleObject = context->WaitForSingleObject;
+    sysmon->CloseHandle         = context->CloseHandle;
+    return true;
+}
+
+// CANNOT merge updateSysmonPointer and recoverSysmonPointer
+// to one function with two arguments, otherwise the compiler
+// will generate the incorrect instructions.
+
+__declspec(noinline)
+static bool updateSysmonPointer(Sysmon* sysmon)
+{
+
+}
+
+__declspec(noinline)
+static bool recoverSysmonPointer(Sysmon* sysmon)
+{
+
+}
+
+__declspec(noinline)
+static bool initSysmonEnvironment(Sysmon* sysmon, Context* context)
+{
+
+}
+
+__declspec(noinline)
+static void eraseSysmonMethods(Context* context)
+{
+
+}
+
+__declspec(noinline)
+static void cleanSysmon(Sysmon* sysmon)
+{
+
 }

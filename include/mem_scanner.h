20087187b1ebfd83eebc621e7ccf91a6819157c8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-28 16:09:44 +0800

    mem_scanner.h: improve comment about MemScanByPattern, ad BinToPattern.

--- include/mem_scanner.h
+++ include/mem_scanner.h
@@ -3,9 +3,19 @@
 
 #include "c_types.h"
 
-// MemScan is used to scans data in the memory of the current process.
-// The return value is the number of results scanned, if return -1,
-// use the GetLastErrno for get error code.
-uint MemScan(byte* pattern, uintptr* results, uint maxItem);
+// MemScanByValue is used to scan data by exact value in the memory
+// of the current process.
+// The return value is the number of results scanned, if failed to
+// scan, it will return -1, use the GetLastErrno for get error code.
+uint MemScanByValue(void* value, uint size, uintptr* results, uint maxItem);
+
+// MemScanByPattern is used to scan data by pattern in the memory
+// of the current process.
+// The return value is the number of results scanned, if failed to
+// scan, it will return -1, use the GetLastErrno for get error code.
+uint MemScanByPattern(byte* pattern, uintptr* results, uint maxItem);
+
+// BinToPattern is used to convert binary data to pattern for MemScan.
+void BinToPattern(void* data, uint size, byte* pattern);
 
 #endif // MEM_SCANNER_H

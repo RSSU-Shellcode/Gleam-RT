ac71d56c63bcfbae06397a5bcbf39c0a74db7d11
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-04-29 15:32:20 +0800

    thread.h: add header file about camouflage methods for thread.

---
+++ include/thread.h
@@ -0,0 +1,11 @@
+#ifndef THREAD_H
+#define THREAD_H
+
+#include "c_types.h"
+
+// CamouflageStartAddress is used to camouflage thread
+// start address, it will return a random address at the
+// text section of current executable image.
+void* CamouflageStartAddress(void* address);
+
+#endif // THREAD_H

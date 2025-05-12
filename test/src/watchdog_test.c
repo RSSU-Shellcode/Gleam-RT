b7d9e9b55926f42d15a238a1ae1e0110297a73b5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-05-12 13:52:03 +0800

    src/watchdog_test.c: finish TestWatchdog_GetStatus, Pause and Continue.

--- test/src/watchdog_test.c
+++ test/src/watchdog_test.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include "c_types.h"
+#include "lib_memory.h"
+#include "watchdog.h"
 #include "test.h"
 
+static bool TestWatchdog_Watcher();
+static bool TestWatchdog_GetStatus();
 static bool TestWatchdog_Pause();
 static bool TestWatchdog_Continue();
 
 bool TestRuntime_Watchdog()
 {
     test_t tests[] = {
-        { TestWatchdog_Pause },
-        { TestWatchdog_Continue },
+        { TestWatchdog_Watcher   },
+        { TestWatchdog_GetStatus },
+        { TestWatchdog_Pause     },
+        { TestWatchdog_Continue  },
     };
     for (int i = 0; i < arrlen(tests); i++)
     {
@@ -23,14 +29,55 @@ bool TestRuntime_Watchdog()
     return true;
 }
 
+static bool TestWatchdog_Watcher()
+{
+    printf_s("test Watchdog_Watcher passed\n");
+    return true;
+}
+
+static bool TestWatchdog_GetStatus()
+{
+    WD_Status status;
+    mem_init(&status, sizeof(status));
+
+    if (!runtime->Watchdog.Status(&status))
+    {
+        printf_s("failed to get watchdog status\n");
+        return false;
+    }
+
+    // if (status.NumNormal < 1)
+    // {
+    //     printf_s("invalid the number of normal\n");
+    //     return false;
+    // }
+
+    printf_s("test TestWatchdog_GetStatus passed\n");
+    return true;
+}
+
 static bool TestWatchdog_Pause()
 {
+    errno errno = runtime->Watchdog.Pause();
+    if (errno != NO_ERROR)
+    {
+        printf_s("failed to pause watchdog: 0x%X\n", errno);
+        return true;
+    }
+
     printf_s("test Watchdog_Pause passed\n");
     return true;
 }
 
 static bool TestWatchdog_Continue()
 {
+    errno errno = runtime->Watchdog.Continue();
+    if (errno != NO_ERROR)
+    {
+        printf_s("failed to continue watchdog: 0x%X\n", errno);
+        return true;
+    }
+
     printf_s("test Watchdog_Continue passed\n");
     return true;
 }

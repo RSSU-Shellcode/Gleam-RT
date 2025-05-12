9bd7b66ba2b676b24d435c2bd2f1abb03816788a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-05-12 13:47:39 +0800

    src/sysmon_test.c: add TestSysmon_GetStatus, finish Pause and Continue.

--- test/src/sysmon_test.c
+++ test/src/sysmon_test.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include "c_types.h"
+#include "lib_memory.h"
+#include "sysmon.h"
 #include "test.h"
 
+static bool TestSysmon_GetStatus();
 static bool TestSysmon_Pause();
 static bool TestSysmon_Continue();
 
 bool TestRuntime_Sysmon()
 {
     test_t tests[] = {
-        { TestSysmon_Pause    },
-        { TestSysmon_Continue },
+        { TestSysmon_GetStatus },
+        { TestSysmon_Pause     },
+        { TestSysmon_Continue  },
     };
     for (int i = 0; i < arrlen(tests); i++)
     {
@@ -23,14 +27,49 @@ bool TestRuntime_Sysmon()
     return true;
 }
 
+static bool TestSysmon_GetStatus()
+{
+    SM_Status status;
+    mem_init(&status, sizeof(status));
+
+    if (!runtime->Sysmon.Status(&status))
+    {
+        printf_s("failed to get sysmon status\n");
+        return false;
+    }
+
+    if (status.NumNormal < 1)
+    {
+        printf_s("invalid the number of normal\n");
+        return false;
+    }
+
+    printf_s("test Sysmon_GetStatus passed\n");
+    return true;
+}
+
 static bool TestSysmon_Pause()
 {
+    errno errno = runtime->Sysmon.Pause();
+    if (errno != NO_ERROR)
+    {
+        printf_s("failed to pause sysmon: 0x%X\n", errno);
+        return true;
+    }
+
     printf_s("test Sysmon_Pause passed\n");
     return true;
 }
 
 static bool TestSysmon_Continue()
 {
+    errno errno = runtime->Sysmon.Continue();
+    if (errno != NO_ERROR)
+    {
+        printf_s("failed to continue sysmon: 0x%X\n", errno);
+        return true;
+    }
+
     printf_s("test Sysmon_Continue passed\n");
     return true;
 }

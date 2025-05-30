88cbaa5e28176cc63e4a467baaf583e6a607497f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-05-30 15:02:41 +0800

    runtime.go: move the code about Metrics to GRT-Develop, add NewRuntime.

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 	"time"
 	"unsafe"
+
+	"github.com/RSSU-Shellcode/GRT-Develop/metric"
 )
 
 const (
@@ -21,65 +23,6 @@ type errno struct {
 
 func (e *errno) Error() string {
 	return fmt.Sprintf("RuntimeM.%s return errno: 0x%08X", e.method, e.errno)
-}
-
-// LTStatus contains status about library tracker.
-type LTStatus struct {
-	NumModules int64 `toml:"num_modules" json:"num_modules"`
-}
-
-// MTStatus contains status about memory tracker.
-type MTStatus struct {
-	NumGlobals int64 `toml:"num_globals" json:"num_globals"`
-	NumLocals  int64 `toml:"num_locals"  json:"num_locals"`
-	NumBlocks  int64 `toml:"num_blocks"  json:"num_blocks"`
-	NumRegions int64 `toml:"num_regions" json:"num_regions"`
-	NumPages   int64 `toml:"num_pages"   json:"num_pages"`
-	NumHeaps   int64 `toml:"num_heaps"   json:"num_heaps"`
-}
-
-// TTStatus contains status about thread tracker.
-type TTStatus struct {
-	NumThreads  int64 `toml:"num_threads"   json:"num_threads"`
-	NumTLSIndex int64 `toml:"num_tls_index" json:"num_tls_index"`
-	NumSuspend  int64 `toml:"num_suspend"   json:"num_suspend"`
-}
-
-// RTStatus contains status about resource tracker.
-type RTStatus struct {
-	NumMutexs         int64 `toml:"num_mutexs"          json:"num_mutexs"`
-	NumEvents         int64 `toml:"num_events"          json:"num_events"`
-	NumSemaphores     int64 `toml:"num_semaphores"      json:"num_semaphores"`
-	NumWaitableTimers int64 `toml:"num_waitable_timers" json:"num_waitable_timers"`
-	NumFiles          int64 `toml:"num_files"           json:"num_files"`
-	NumDirectories    int64 `toml:"num_directories"     json:"num_directories"`
-	NumIOCPs          int64 `toml:"num_iocps"           json:"num_iocps"`
-	NumKeys           int64 `toml:"num_keys"            json:"num_keys"`
-	NumSockets        int64 `toml:"num_sockets"         json:"num_sockets"`
-}
-
-// SMStatus contains status about sysmon.
-type SMStatus struct {
-	NumNormal  int64 `toml:"num_normal"  json:"num_normal"`
-	NumRecover int64 `toml:"num_recover" json:"num_recover"`
-	NumPanic   int64 `toml:"num_panic"   json:"num_panic"`
-}
-
-// WDStatus contains status about watchdog.
-type WDStatus struct {
-	NumKick   int64 `toml:"num_kick"   json:"num_kick"`
-	NumNormal int64 `toml:"num_normal" json:"num_normal"`
-	NumReset  int64 `toml:"num_reset"  json:"num_reset"`
-}
-
-// Metrics contains status about runtime submodules.
-type Metrics struct {
-	Library  LTStatus `toml:"library"  json:"library"`
-	Memory   MTStatus `toml:"memory"   json:"memory"`
-	Thread   TTStatus `toml:"thread"   json:"thread"`
-	Resource RTStatus `toml:"resource" json:"resource"`
-	Sysmon   SMStatus `toml:"sysmon"   json:"sysmon"`
-	Watchdog WDStatus `toml:"watchdog" json:"watchdog"`
 }
 
 // Options contains options about initialize runtime.
@@ -276,6 +219,11 @@ type RuntimeM struct {
 	ExitProcess uintptr
 }
 
+// NewRuntime is used to create runtime from initialized instance.
+func NewRuntime(addr uintptr) *RuntimeM {
+	return (*RuntimeM)(unsafe.Pointer(addr)) // #nosec
+}
+
 // InitRuntime is used to initialize runtime from shellcode instance.
 // Each shellcode instance can only initialize once.
 func InitRuntime(addr uintptr, opts *Options) (*RuntimeM, error) {
@@ -283,7 +231,7 @@ func InitRuntime(addr uintptr, opts *Options) (*RuntimeM, error) {
 	if ret == null {
 		return nil, fmt.Errorf("failed to initialize runtime: 0x%X", err)
 	}
-	return (*RuntimeM)(unsafe.Pointer(ret)), nil // #nosec
+	return NewRuntime(ret), nil
 }
 
 // Sleep is used to sleep and hide runtime.
@@ -296,8 +244,8 @@ func (rt *RuntimeM) Sleep(d time.Duration) error {
 }
 
 // Metrics is used to get runtime metric about core modules.
-func (rt *RuntimeM) Metrics() (*Metrics, error) {
-	metrics := Metrics{}
+func (rt *RuntimeM) Metrics() (*metric.Metrics, error) {
+	metrics := metric.Metrics{}
 	ret, _, _ := syscall.SyscallN(rt.Core.Metrics, uintptr(unsafe.Pointer(&metrics))) // #nosec
 	if ret != noError {
 		return nil, &errno{method: "Core.Metrics", errno: ret}

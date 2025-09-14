8faae3e44d61d577e1a2aeb4f614d09744141d0b
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-09-14 12:45:35 +0800

    lib_string.h: replace the Unicode to UTF-16 in the comment.

--- include/lib_string.h
+++ include/lib_string.h
@@ -9,7 +9,7 @@ typedef uint16* UTF16;
 // strlen_a is used to calculate ANSI string length.
 uint strlen_a(ANSI s);
 
-// strlen_w is used to calculate Unicode string length.
+// strlen_w is used to calculate UTF-16 string length.
 uint strlen_w(UTF16 s);
 
 // strcmp_a is used to compare two ANSI strings.
@@ -18,7 +18,7 @@ uint strlen_w(UTF16 s);
 // if a < b, return -1
 int strcmp_a(ANSI a, ANSI b);
 
-// strcmp_w is used to compare two Unicode strings.
+// strcmp_w is used to compare two UTF-16 strings.
 // if a = b, return 0
 // if a > b, return 1
 // if a < b, return -1
@@ -30,7 +30,7 @@ int strcmp_w(UTF16 a, UTF16 b);
 // if a < b, return -1
 int strncmp_a(ANSI a, ANSI b, int64 n);
 
-// strncmp_w is used to compare two Unicode strings with length.
+// strncmp_w is used to compare two UTF-16 strings with length.
 // if a = b, return 0
 // if a > b, return 1
 // if a < b, return -1
@@ -43,7 +43,7 @@ int strncmp_w(UTF16 a, UTF16 b, int64 n);
 // if a < b, return -1
 int stricmp_a(ANSI a, ANSI b);
 
-// stricmp_w is used to compare two Unicode strings, it is case-insensitive.
+// stricmp_w is used to compare two UTF-16 strings, it is case-insensitive.
 // if a = b, return 0
 // if a > b, return 1
 // if a < b, return -1
@@ -55,7 +55,7 @@ int stricmp_w(UTF16 a, UTF16 b);
 // if a < b, return -1
 int strnicmp_a(ANSI a, ANSI b, int64 n);
 
-// strnicmp_w is used to compare two Unicode strings with length, it is case-insensitive.
+// strnicmp_w is used to compare two UTF-16 strings with length, it is case-insensitive.
 // if a = b, return 0
 // if a > b, return 1
 // if a < b, return -1
@@ -64,13 +64,13 @@ int strnicmp_w(UTF16 a, UTF16 b, int64 n);
 // strcpy_a is used to copy source ANSI string to destination.
 uint strcpy_a(ANSI dst, ANSI src);
 
-// strcpy_w is used to copy source Unicode string to destination.
+// strcpy_w is used to copy source UTF-16 string to destination.
 uint strcpy_w(UTF16 dst, UTF16 src);
 
 // strcpy_a is used to copy source ANSI string to destination with length.
 uint strncpy_a(ANSI dst, ANSI src, int64 n);
 
-// strcpy_w is used to copy source Unicode string to destination with length.
+// strcpy_w is used to copy source UTF-16 string to destination with length.
 uint strncpy_w(UTF16 dst, UTF16 src, int64 n);
 
 #endif // LIB_STRING_H

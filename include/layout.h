f65daeacb2d7c1f263653524eb526ce768b5528e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-01 14:45:01 +0800

    layout.h: add #define about runtime core and tracker modules.

--- include/layout.h
+++ include/layout.h
@@ -7,8 +7,19 @@
 // | runtime core | runtime submodules | high-level module |
 // +--------------+--------------------+-------------------+
 
-#define MAIN_MEM_PAGE_SIZE (32 * 1024)
+#define MAIN_MEM_PAGE_SIZE (8 * 4096)
 
+// runtime core
+#define LAYOUT_RUNTIME_STRUCT 1000
+#define LAYOUT_RUNTIME_MODULE 2800
 
+// submodule
+#define LAYOUT_LT_STRUCT 4096
+#define LAYOUT_LT_MODULE 5000
+
+#define LAYOUT_MT_STRUCT 6000
+#define LAYOUT_MT_MODULE 7000
+
+// high-level module
 
 #endif // LAYOUT_H

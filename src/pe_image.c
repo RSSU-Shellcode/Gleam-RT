8eb47917b2a1fd63c14bdd1ae3599901cb41b793
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-09 15:20:38 +0800

    pe_image.c: improve the ParsePEImage about reuse peHeader address.

--- src/pe_image.c
+++ src/pe_image.c
@@ -4,21 +4,28 @@
 #include "crypto.h"
 #include "pe_image.h"
 
+#define DOS_HEADER_SIZE 64
+
+#define NT_HEADER_SIGNATURE_SIZE 4
+#define PE_FILE_HEADER_SIZE      20
+#define PE_SECTION_HEADER_SIZE   40
+
 void ParsePEImage(void* address, PE_Image* image)
 {
     uintptr imageAddr = (uintptr)address;
-    uint32  peOffset  = *(uint32*)(imageAddr + 60);
+    uint32  peOffset  = *(uint32*)(imageAddr + DOS_HEADER_SIZE - 4);
+    uintptr peHeader  = imageAddr + peOffset + NT_HEADER_SIGNATURE_SIZE;
     // parse file header
-    uint16 numSections   = *(uint16*)(imageAddr + peOffset + 6);
-    uint16 optHeaderSize = *(uint16*)(imageAddr + peOffset + 20);
+    uint16 numSections   = *(uint16*)(peHeader + 2);
+    uint16 optHeaderSize = *(uint16*)(peHeader + 16);
     // parse optional header
-    uint32  entryPoint = *(uint32*)(imageAddr + peOffset + 40);
+    uint32  entryPoint = *(uint32*)(peHeader + 36);
 #ifdef _WIN64
-    uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 48);
+    uintptr imageBase = *(uintptr*)(peHeader + 44);
 #elif _WIN32
-    uintptr imageBase = *(uintptr*)(imageAddr + peOffset + 52);
+    uintptr imageBase = *(uintptr*)(peHeader + 48);
 #endif
-    uint32 imageSize = *(uint32*)(imageAddr + peOffset + 80);
+    uint32 imageSize = *(uint32*)(peHeader + 76);
     // not record the original ".text" bytes
     byte target[] = {
         '.'^0x19, 't'^0xF4, 'e'^0xBF, 'x'^0x8C,
@@ -27,7 +34,7 @@ void ParsePEImage(void* address, PE_Image* image)
     byte key[] = {0x19, 0xF4, 0xBF, 0x8C};
     XORBuf(target, sizeof(target), key, sizeof(key));
     // parse sections and search .text
-    uintptr section = imageAddr + PE_FILE_HEADER_SIZE + peOffset + optHeaderSize;
+    uintptr section = peHeader + PE_FILE_HEADER_SIZE + optHeaderSize;
     for (uint16 i = 0; i < numSections; i++)
     {
         if (strncmp_a((ANSI)section, (ANSI)target, sizeof(target)) != 0)
@@ -35,10 +42,14 @@ void ParsePEImage(void* address, PE_Image* image)
             section += PE_SECTION_HEADER_SIZE;
             continue;
         }
-        image->TextVirtualSize      = *(uint32*)(section + 8); 
-        image->TextVirtualAddress   = *(uint32*)(section + 12); 
-        image->TextSizeOfRawData    = *(uint32*)(section + 16); 
-        image->TextPointerToRawData = *(uint32*)(section + 20);
+        section += 8; // skip section name
+        image->TextVirtualSize      = *(uint32*)(section + 0);
+        image->TextVirtualAddress   = *(uint32*)(section + 4);
+        image->TextSizeOfRawData    = *(uint32*)(section + 8);
+        image->TextPointerToRawData = *(uint32*)(section + 12);
+
+        panic(PANIC_REACHABLE_TEST);
+
         break;
     }
     image->EntryPoint = imageAddr + entryPoint;

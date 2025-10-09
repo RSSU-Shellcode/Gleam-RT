9c1ba6d671ca896daaa795e6e04c55b3e70722ed
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-09 16:04:31 +0800

    pe_image.c: refactor ParsePEImage about constant and PE headers.

--- src/pe_image.c
+++ src/pe_image.c
@@ -4,28 +4,14 @@
 #include "crypto.h"
 #include "pe_image.h"
 
-#define DOS_HEADER_SIZE 64
-
-#define NT_HEADER_SIGNATURE_SIZE 4
-#define PE_FILE_HEADER_SIZE      20
-#define PE_SECTION_HEADER_SIZE   40
-
 void ParsePEImage(void* address, PE_Image* image)
 {
     uintptr imageAddr = (uintptr)address;
-    uint32  peOffset  = *(uint32*)(imageAddr + DOS_HEADER_SIZE - 4);
-    uintptr peHeader  = imageAddr + peOffset + NT_HEADER_SIGNATURE_SIZE;
-    // parse file header
-    uint16 numSections   = *(uint16*)(peHeader + 2);
-    uint16 optHeaderSize = *(uint16*)(peHeader + 16);
-    // parse optional header
-    uint32  entryPoint = *(uint32*)(peHeader + 36);
-#ifdef _WIN64
-    uintptr imageBase = *(uintptr*)(peHeader + 44);
-#elif _WIN32
-    uintptr imageBase = *(uintptr*)(peHeader + 48);
-#endif
-    uint32 imageSize = *(uint32*)(peHeader + 76);
+    uint32  hdrOffset = *(uint32*)(imageAddr + DOS_HEADER_SIZE - 4);
+    // parse PE headers
+    Image_NTHeaders*     ntHeaders  = (Image_NTHeaders*)(imageAddr + hdrOffset);
+    Image_FileHeader     fileHeader = ntHeaders->FileHeader;
+    Image_OptionalHeader optHeader  = ntHeaders->OptionalHeader;
     // not record the original ".text" bytes
     byte target[] = {
         '.'^0x19, 't'^0xF4, 'e'^0xBF, 'x'^0x8C,
@@ -34,25 +20,23 @@ void ParsePEImage(void* address, PE_Image* image)
     byte key[] = {0x19, 0xF4, 0xBF, 0x8C};
     XORBuf(target, sizeof(target), key, sizeof(key));
     // parse sections and search .text
-    uintptr section = peHeader + PE_FILE_HEADER_SIZE + optHeaderSize;
-    for (uint16 i = 0; i < numSections; i++)
+    uintptr fileAddr = imageAddr + hdrOffset + sizeof(ntHeaders->Signature);
+    uintptr optAddr  = fileAddr + sizeof(Image_FileHeader);
+    uint32  optSize  = fileHeader.SizeOfOptionalHeader;
+    Image_SectionHeader* section = (Image_SectionHeader*)(optAddr + optSize);
+    for (uint16 i = 0; i < fileHeader.NumberOfSections; i++)
     {
         if (strncmp_a((ANSI)section, (ANSI)target, sizeof(target)) != 0)
         {
-            section += PE_SECTION_HEADER_SIZE;
+            section++;
             continue;
         }
-        section += 8; // skip section name
-        image->TextVirtualSize      = *(uint32*)(section + 0);
-        image->TextVirtualAddress   = *(uint32*)(section + 4);
-        image->TextSizeOfRawData    = *(uint32*)(section + 8);
-        image->TextPointerToRawData = *(uint32*)(section + 12);
-
-        panic(PANIC_REACHABLE_TEST);
-
+        image->Text = *section;
         break;
     }
-    image->EntryPoint = imageAddr + entryPoint;
-    image->ImageBase  = imageBase;
-    image->ImageSize  = imageSize;
+    image->EntryPoint     = imageAddr + optHeader.AddressOfEntryPoint;
+    image->ImageBase      = optHeader.ImageBase;
+    image->ImageSize      = optHeader.SizeOfImage;
+    image->FileHeader     = fileHeader;
+    image->OptionalHeader = optHeader;
 }

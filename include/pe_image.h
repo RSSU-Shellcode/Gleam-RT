cd0c81e9d589d6f2572cdeecd33af5d10c5509b2
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-10-09 16:03:11 +0800

    pe_image.h: add structure about PE image, update PE_Image.

--- include/pe_image.h
+++ include/pe_image.h
@@ -2,18 +2,128 @@
 #define PE_IMAGE_H
 
 #include "c_types.h"
+#include "win_types.h"
+
+#define DOS_HEADER_SIZE 64
 
 typedef struct {
-    // optional header
+    WORD  Machine;
+    WORD  NumberOfSections;
+    DWORD TimeDateStamp;
+    DWORD PointerToSymbolTable;
+    DWORD NumberOfSymbols;
+    WORD  SizeOfOptionalHeader;
+    WORD  Characteristics;
+} Image_FileHeader;
+
+typedef struct {
+    DWORD VirtualAddress;
+    DWORD Size;
+} Image_DataDirectory;
+
+typedef struct {
+    WORD  Magic;
+    BYTE  MajorLinkerVersion;
+    BYTE  MinorLinkerVersion;
+    DWORD SizeOfCode;
+    DWORD SizeOfInitializedData;
+    DWORD SizeOfUninitializedData;
+    DWORD AddressOfEntryPoint;
+    DWORD BaseOfCode;
+    DWORD BaseOfData;
+    DWORD ImageBase;
+    DWORD SectionAlignment;
+    DWORD FileAlignment;
+    WORD  MajorOperatingSystemVersion;
+    WORD  MinorOperatingSystemVersion;
+    WORD  MajorImageVersion;
+    WORD  MinorImageVersion;          
+    WORD  MajorSubsystemVersion;
+    WORD  MinorSubsystemVersion;      
+    DWORD Win32VersionValue;      
+    DWORD SizeOfImage;          
+    DWORD SizeOfHeaders;
+    DWORD CheckSum;             
+    WORD  Subsystem;                  
+    WORD  DllCharacteristics;
+    DWORD SizeOfStackReserve;        
+    DWORD SizeOfStackCommit;        
+    DWORD SizeOfHeapReserve;         
+    DWORD SizeOfHeapCommit;         
+    DWORD LoaderFlags;          
+    DWORD NumberOfRvaAndSizes;
+
+    Image_DataDirectory DataDirectory[16];
+} Image_OptionalHeader32;
+
+typedef struct {
+    WORD  Magic;
+    BYTE  MajorLinkerVersion;
+    BYTE  MinorLinkerVersion;
+    DWORD SizeOfCode;
+    DWORD SizeOfInitializedData;
+    DWORD SizeOfUninitializedData;
+    DWORD AddressOfEntryPoint;
+    DWORD BaseOfCode;
+    QWORD ImageBase;
+    DWORD SectionAlignment;
+    DWORD FileAlignment;
+    WORD  MajorOperatingSystemVersion;
+    WORD  MinorOperatingSystemVersion;
+    WORD  MajorImageVersion;
+    WORD  MinorImageVersion;
+    WORD  MajorSubsystemVersion;
+    WORD  MinorSubsystemVersion;      
+    DWORD Win32VersionValue;      
+    DWORD SizeOfImage;          
+    DWORD SizeOfHeaders;
+    DWORD CheckSum;              
+    WORD  Subsystem;
+    WORD  DllCharacteristics;
+    QWORD SizeOfStackReserve;
+    QWORD SizeOfStackCommit;
+    QWORD SizeOfHeapReserve;
+    QWORD SizeOfHeapCommit;
+    DWORD LoaderFlags;
+    DWORD NumberOfRvaAndSizes;
+
+    Image_DataDirectory DataDirectory[16];
+} Image_OptionalHeader64;
+
+#ifdef _WIN64
+    typedef Image_OptionalHeader64 Image_OptionalHeader;
+#elif _WIN32
+    typedef Image_OptionalHeader32 Image_OptionalHeader;
+#endif
+
+typedef struct {
+    DWORD                Signature;
+    Image_FileHeader     FileHeader;
+    Image_OptionalHeader OptionalHeader;
+} Image_NTHeaders;
+
+typedef struct {
+    BYTE  Name[8];
+    DWORD VirtualSize;
+    DWORD VirtualAddress;
+    DWORD SizeOfRawData;
+    DWORD PointerToRawData;
+    DWORD PointerToRelocations;
+    DWORD PointerToLineNumbers;
+    WORD  NumberOfRelocations;
+    WORD  NumberOfLineNumbers;
+    DWORD Characteristics;
+} Image_SectionHeader;
+
+typedef struct {
     uintptr EntryPoint;
     uintptr ImageBase;
     uint32  ImageSize;
 
-    // section information
-    uint32 TextVirtualSize;
-    uint32 TextVirtualAddress;
-    uint32 TextSizeOfRawData;
-    uint32 TextPointerToRawData;
+    Image_FileHeader     FileHeader;
+    Image_OptionalHeader OptionalHeader;
+
+    Image_SectionHeader Text;
 } PE_Image;
 
 void ParsePEImage(void* address, PE_Image* image);

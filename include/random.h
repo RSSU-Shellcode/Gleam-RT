edbe9038f39f94e71e9a7e7372f3631516e938f9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-11-27 14:49:25 +0800

    random.h: add more methods for generate various random type.

--- include/random.h
+++ include/random.h
@@ -6,20 +6,32 @@
 // [reference]
 // https://en.wikipedia.org/wiki/xorshift
 
-// RandByte is used to generate random byte.
-byte RandByte(uint64 seed);
-
-// RandBool is used to generate random bool.
-BOOL RandBool(uint64 seed);
-
 // RandInt is used to generate random int.
 int RandInt(uint64 seed);
+
+// RandInt8 is used to generate random int8.
+int8 RandInt8(uint64 seed);
+
+// RandInt16 is used to generate random int16.
+int16 RandInt16(uint64 seed);
+
+// RandInt32 is used to generate random int32.
+int32 RandInt32(uint64 seed);
+
+// RandInt64 is used to generate random int64.
+int64 RandInt64(uint64 seed);
 
 // RandUint is used to generate random uint.
 uint RandUint(uint64 seed);
 
-// RandInt64 is used to generate random int64.
-int64 RandInt64(uint64 seed);
+// RandUint8 is used to generate random uint8.
+uint8 RandUint8(uint64 seed);
+
+// RandUint16 is used to generate random uint16.
+uint16 RandUint16(uint64 seed);
+
+// RandUint32 is used to generate random uint32.
+uint32 RandUint32(uint64 seed);
 
 // RandUint64 is used to generate random uint64.
 uint64 RandUint64(uint64 seed);
@@ -27,14 +39,41 @@ uint64 RandUint64(uint64 seed);
 // RandIntN is used to generate random int with range.
 int RandIntN(uint64 seed, int n);
 
-// RandUintN is used to generate random uint with range.
-uint RandUintN(uint64 seed, uint n);
+// RandInt8N is used to generate random int8 with range.
+int8 RandInt8N(uint64 seed, int8 n);
+
+// RandInt16N is used to generate random int16 with range.
+int16 RandInt16N(uint64 seed, int16 n);
+
+// RandInt32N is used to generate random int32 with range.
+int32 RandInt32N(uint64 seed, int32 n);
 
 // RandInt64N is used to generate random int64 with range.
 int64 RandInt64N(uint64 seed, int64 n);
 
+// RandUintN is used to generate random uint with range.
+uint RandUintN(uint64 seed, uint n);
+
+// RandUint8N is used to generate random uint8 with range.
+uint8 RandUint8N(uint64 seed, uint8 n);
+
+// RandUint16N is used to generate random uint16 with range.
+uint16 RandUint16N(uint64 seed, uint16 n);
+
+// RandUint32N is used to generate random uint32 with range.
+uint32 RandUint32N(uint64 seed, uint32 n);
+
 // RandUint64N is used to generate random uint64 with range.
 uint64 RandUint64N(uint64 seed, uint64 n);
+
+// RandByte is used to generate random byte.
+byte RandByte(uint64 seed);
+
+// RandBool is used to generate random bool.
+bool RandBool(uint64 seed);
+
+// RandBOOL is used to generate random BOOL.
+BOOL RandBOOL(uint64 seed);
 
 // RandBuffer is used to fill random bytes to the memory.
 void RandBuffer(void* buf, int64 size);

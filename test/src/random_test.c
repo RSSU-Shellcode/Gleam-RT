6132b4bda4046c6e57be20ef3a5513898595bc70
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-11-27 15:15:53 +0800

    src/random_test.c: adjust unit test order, add more tests.

--- test/src/random_test.c
+++ test/src/random_test.c
@@ -4,16 +4,27 @@
 #include "test.h"
 
 static void TestGenerateSeed();
-static void TestRandByte();
-static void TestRandBool();
 static void TestRandInt();
-static void TestRandUint();
+static void TestRandInt8();
+static void TestRandInt16();
+static void TestRandInt32();
 static void TestRandInt64();
+static void TestRandUint8();
+static void TestRandUint16();
+static void TestRandUint32();
 static void TestRandUint64();
 static void TestRandIntN();
-static void TestRandUintN();
+static void TestRandInt8N();
+static void TestRandInt16N();
+static void TestRandInt32N();
 static void TestRandInt64N();
+static void TestRandUint8N();
+static void TestRandUint16N();
+static void TestRandUint32N();
 static void TestRandUint64N();
+static void TestRandByte();
+static void TestRandBool();
+static void TestRandBOOL();
 static void TestRandBuffer();
 static void TestRandSequence();
 
@@ -23,16 +34,27 @@ bool TestRandom()
     test_t tests[] = 
     {
         { TestGenerateSeed },
-        { TestRandByte     },
-        { TestRandBool     },
         { TestRandInt      },
-        { TestRandUint     },
+        { TestRandInt8     },
+        { TestRandInt16    },
+        { TestRandInt32    },
         { TestRandInt64    },
+        { TestRandUint8    },
+        { TestRandUint16   },
+        { TestRandUint32   },
         { TestRandUint64   },
         { TestRandIntN     },
-        { TestRandUintN    },
+        { TestRandInt8N    },
+        { TestRandInt16N   },
+        { TestRandInt32N   },
         { TestRandInt64N   },
+        { TestRandUint8N   },
+        { TestRandUint16N  },
+        { TestRandUint32N  },
         { TestRandUint64N  },
+        { TestRandByte     },
+        { TestRandBool     },
+        { TestRandBOOL     },
         { TestRandBuffer   },
         { TestRandSequence },
     };
@@ -53,6 +75,170 @@ static void TestGenerateSeed()
     }
 
     printf_s("======TestGenerateSeed passed======\n\n");
+}
+
+static void TestRandInt()
+{
+    printf_s("=========TestRandInt begin=========\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        uint64 seed = GenerateSeed();
+        printf_s("int: %lld\n", (uint64)RandInt(seed));
+    }
+    printf_s("\n");
+
+    // iteration
+    uint64 last = (uint64)(&TestRandBuffer);
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 val = (uint64)RandInt(last);
+        printf_s("int: %lld\n", val);
+        last += val;
+    }
+
+    printf_s("=========TestRandInt passed========\n\n");
+}
+
+static void TestRandInt64()
+{
+    printf_s("========TestRandInt64 begin========\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        uint64 seed = GenerateSeed();
+        printf_s("int64: %lld\n", (uint64)RandInt64(seed));
+    }
+    printf_s("\n");
+
+    // iteration
+    uint64 last = (uint64)(&TestRandBuffer);
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 val = (uint64)RandInt64(last);
+        printf_s("int64: %lld\n", val);
+        last += val;
+    }
+
+    printf_s("========TestRandInt64 passed=======\n\n");
+}
+
+static void TestRandUint()
+{
+    printf_s("=========TestRandUint begin========\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        uint64 seed = GenerateSeed();
+        printf_s("uint: %llu\n", (uint64)RandUint(seed));
+    }
+    printf_s("\n");
+
+    // iteration
+    uint64 last = (uint64)(&TestRandBuffer);
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 val = (uint64)RandUint(last);
+        printf_s("uint: %llu\n", val);
+        last += val;
+    }
+
+    printf_s("========TestRandUint passed========\n\n");
+}
+
+static void TestRandUint64()
+{
+    printf_s("=======TestRandUint64 begin========\n");
+
+    for (int i = 0; i < 3; i++)
+    {
+        uint64 seed = GenerateSeed();
+        printf_s("uint64: %llu\n", RandUint64(seed));
+    }
+    printf_s("\n");
+
+    // iteration
+    uint64 last = (uint64)(&TestRandBuffer);
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 val = RandUint64(last);
+        printf_s("uint64: %llu\n", val);
+        last += val;
+    }
+
+    printf_s("=======TestRandUint64 passed=======\n\n");
+}
+
+static void TestRandIntN()
+{
+    printf_s("==========RandIntN begin===========\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 seed = GenerateSeed();
+        uint64 val  = (uint64)RandIntN(seed, 1024);
+        if (val > 1024)
+        {
+            panic(PANIC_UNREACHABLE_CODE);
+        }
+        printf_s("int: %lld\n", val);
+    }
+
+    printf_s("==========RandIntN passed==========\n\n");
+}
+
+static void TestRandInt64N()
+{
+    printf_s("=========RandInt64N begin==========\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 seed = GenerateSeed();
+        uint64 val  = (uint64)RandInt64N(seed, 1024);
+        if (val > 1024)
+        {
+            panic(PANIC_UNREACHABLE_CODE);
+        }
+        printf_s("int64: %lld\n", val);
+    }
+
+    printf_s("=========RandInt64N passed=========\n\n");
+}
+
+static void TestRandUintN()
+{
+    printf_s("=========RandUintN begin===========\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 seed = GenerateSeed();
+        uint64 val  = (uint64)RandUintN(seed, 1024);
+        if (val > 1024)
+        {
+            panic(PANIC_UNREACHABLE_CODE);
+        }
+        printf_s("uint: %llu\n", val);
+    }
+
+    printf_s("=========RandUintN passed==========\n\n");
+}
+
+static void TestRandUint64N()
+{
+    printf_s("========RandUint64N begin==========\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        uint64 seed = GenerateSeed();
+        uint64 val  = (uint64)RandUint64N(seed, 1024);
+        if (val > 1024)
+        {
+            panic(PANIC_UNREACHABLE_CODE);
+        }
+        printf_s("uint64: %llu\n", val);
+    }
+
+    printf_s("========RandUint64N passed=========\n\n");
 }
 
 static void TestRandByte()
@@ -101,14 +287,14 @@ static void TestRandBool()
     printf_s("========TestRandBool passed========\n\n");
 }
 
-static void TestRandInt()
+static void TestRandBOOL()
 {
-    printf_s("=========TestRandInt begin=========\n");
+    printf_s("=========TestRandBOOL begin========\n");
 
     for (int i = 0; i < 3; i++)
     {
         uint64 seed = GenerateSeed();
-        printf_s("int: %lld\n", (uint64)RandInt(seed));
+        printf_s("bool: %d\n", RandBOOL(seed));
     }
     printf_s("\n");
 
@@ -116,153 +302,12 @@ static void TestRandInt()
     uint64 last = (uint64)(&TestRandBuffer);
     for (int i = 0; i < 5; i++)
     {
-        uint64 val = (uint64)RandInt(last);
-        printf_s("int: %lld\n", val);
-        last += val;
+        uint64 val = (uint64)RandBOOL(last);
+        printf_s("bool: %lld\n", val);
+        last += last + val + 1;
     }
 
-    printf_s("=========TestRandInt passed========\n\n");
-}
-
-static void TestRandUint()
-{
-    printf_s("=========TestRandUint begin========\n");
-
-    for (int i = 0; i < 3; i++)
-    {
-        uint64 seed = GenerateSeed();
-        printf_s("uint: %llu\n", (uint64)RandUint(seed));
-    }
-    printf_s("\n");
-
-    // iteration
-    uint64 last = (uint64)(&TestRandBuffer);
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 val = (uint64)RandUint(last);
-        printf_s("uint: %llu\n", val);
-        last += val;
-    }
-
-    printf_s("========TestRandUint passed========\n\n");
-}
-
-static void TestRandInt64()
-{
-    printf_s("========TestRandInt64 begin========\n");
-
-    for (int i = 0; i < 3; i++)
-    {
-        uint64 seed = GenerateSeed();
-        printf_s("int64: %lld\n", (uint64)RandInt64(seed));
-    }
-    printf_s("\n");
-
-    // iteration
-    uint64 last = (uint64)(&TestRandBuffer);
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 val = (uint64)RandInt64(last);
-        printf_s("int64: %lld\n", val);
-        last += val;
-    }
-
-    printf_s("========TestRandInt64 passed=======\n\n");
-}
-
-static void TestRandUint64()
-{
-    printf_s("=======TestRandUint64 begin========\n");
-
-    for (int i = 0; i < 3; i++)
-    {
-        uint64 seed = GenerateSeed();
-        printf_s("uint64: %llu\n", RandUint64(seed));
-    }
-    printf_s("\n");
-
-    // iteration
-    uint64 last = (uint64)(&TestRandBuffer);
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 val = RandUint64(last);
-        printf_s("uint64: %llu\n", val);
-        last += val;
-    }
-
-    printf_s("=======TestRandUint64 passed=======\n\n");
-}
-
-static void TestRandIntN()
-{
-    printf_s("==========RandIntN begin===========\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 seed = GenerateSeed();
-        uint64 val  = (uint64)RandIntN(seed, 1024);
-        if (val > 1024)
-        {
-            panic(PANIC_UNREACHABLE_CODE);
-        }
-        printf_s("int: %lld\n", val);
-    }
-
-    printf_s("==========RandIntN passed==========\n\n");
-}
-
-static void TestRandUintN()
-{
-    printf_s("=========RandUintN begin===========\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 seed = GenerateSeed();
-        uint64 val  = (uint64)RandUintN(seed, 1024);
-        if (val > 1024)
-        {
-            panic(PANIC_UNREACHABLE_CODE);
-        }
-        printf_s("uint: %llu\n", val);
-    }
-
-    printf_s("=========RandUintN passed==========\n\n");
-}
-
-static void TestRandInt64N()
-{
-    printf_s("=========RandInt64N begin==========\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 seed = GenerateSeed();
-        uint64 val  = (uint64)RandInt64N(seed, 1024);
-        if (val > 1024)
-        {
-            panic(PANIC_UNREACHABLE_CODE);
-        }
-        printf_s("int64: %lld\n", val);
-    }
-
-    printf_s("=========RandInt64N passed=========\n\n");
-}
-
-static void TestRandUint64N()
-{
-    printf_s("========RandUint64N begin==========\n");
-
-    for (int i = 0; i < 5; i++)
-    {
-        uint64 seed = GenerateSeed();
-        uint64 val  = (uint64)RandUint64N(seed, 1024);
-        if (val > 1024)
-        {
-            panic(PANIC_UNREACHABLE_CODE);
-        }
-        printf_s("uint64: %llu\n", val);
-    }
-
-    printf_s("========RandUint64N passed=========\n\n");
+    printf_s("========TestRandBOOL passed========\n\n");
 }
 
 static void TestRandBuffer()

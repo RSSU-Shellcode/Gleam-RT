c4e093d384daabbf87be5185f5a13a608f39997f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-11-29 14:31:40 +0800

    mem_scanner.h: add MemScan_Cfg for control scan memory region Protect and Type.

--- include/mem_scanner.h
+++ include/mem_scanner.h
@@ -2,6 +2,7 @@
 #define MEM_SCANNER_H
 
 #include "c_types.h"
+#include "win_types.h"
 #include "dll_kernel32.h"
 
 typedef struct {
@@ -11,19 +12,31 @@ typedef struct {
     VirtualQuery_t VirtualQuery;
 } MemScan_Ctx;
 
+typedef struct {
+    byte* Pattern;
+    DWORD Protect; // default PAGE_READWRITE
+    DWORD Type;    // default MEM_PRIVATE
+} MemScan_Cfg;
+
 // MemScanByValue is used to scan data by exact value in the memory
 // of the current process.
+// 
+// It will only scan memory page protect with PAGE_READWRITE, if you
+// want to scan other protect, use MemScanByConfig.
+// 
 // The return value is the number of results scanned, if failed to
 // scan, it will return -1, use the GetLastErrno for get error code.
 uint MemScanByValue(MemScan_Ctx* ctx, void* value, uint size, uintptr* results, uint maxItem);
 
-// MemScanByPattern is used to scan data by pattern in the memory
-// of the current process.
+// MemScanByConfig is used to scan data by config like pattern in
+// the memory of the current process.
+// 
 // The return value is the number of results scanned, if failed to
 // scan, it will return -1, use the GetLastErrno for get error code.
-uint MemScanByPattern(MemScan_Ctx* ctx, byte* pattern, uintptr* results, uint maxItem);
+uint MemScanByConfig(MemScan_Ctx* ctx, MemScan_Cfg* config, uintptr* results, uint maxItem);
 
 // BinToPattern is used to convert binary data to pattern for MemScan.
+// The pattern buffer size must greater the [data size * 3 + 1].
 void BinToPattern(void* data, uint size, byte* pattern);
 
 #endif // MEM_SCANNER_H

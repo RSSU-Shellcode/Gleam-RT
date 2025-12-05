a5789d668bacf2bcd4d5239b3c52e3aa9099c1a9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-05 14:04:03 +0800

    test.h: move TestRuntime_Sysmon after TestRuntime_Watchdog.

--- test/test.h
+++ test/test.h
@@ -28,8 +28,8 @@ bool TestRuntime_WinBase();
 bool TestRuntime_WinFile();
 bool TestRuntime_WinHTTP();
 bool TestRuntime_WinCrypto();
-bool TestRuntime_Sysmon();
 bool TestRuntime_Watchdog();
+bool TestRuntime_Sysmon();
 bool TestRuntime_Exit();
 bool TestRuntime_Options();
 #pragma warning(pop)
@@ -58,8 +58,8 @@ static unit tests[] =
     { "Runtime_WinFile",   TestRuntime_WinFile   },
     { "Runtime_WinHTTP",   TestRuntime_WinHTTP   },
     { "Runtime_WinCrypto", TestRuntime_WinCrypto },
-    { "Runtime_Sysmon",    TestRuntime_Sysmon    },
     { "Runtime_Watchdog",  TestRuntime_Watchdog  },
+    { "Runtime_Sysmon",    TestRuntime_Sysmon    },
     { "Runtime_Exit",      TestRuntime_Exit      },
     { "Runtime_Options",   TestRuntime_Options   },
 };

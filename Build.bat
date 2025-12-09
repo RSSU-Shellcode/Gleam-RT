e7876ca088a2414c9c6374f96185995440fda3e1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-09 14:53:04 +0800

    add section about generate test dll, update sections about clean output files.

--- Build.bat
+++ Build.bat
@@ -7,11 +7,13 @@ if "%VisualStudio%" == "" (
 )
 call "%VisualStudio%\VC\Auxiliary\Build\vcvars64.bat"
 
-echo ================= clean builder old files ================
-rd /S /Q "builder\Release"
-rd /S /Q "builder\x64"
+echo ==================== clean old files =====================
 rd /S /Q "Release"
 rd /S /Q "x64"
+rd /S /Q "builder\Release"
+rd /S /Q "builder\x64"
+rd /S /Q "tool\test_dll\Release"
+rd /S /Q "tool\test_dll\x64"
 
 echo ==================== generate builder ====================
 MSBuild.exe Gleam-RT.sln /t:builder /p:Configuration=Release /p:Platform=x86
@@ -26,16 +28,24 @@ echo --------extract shellcode for x64--------
 "..\x64\Release\builder.exe"
 cd ..
 
-echo ================ clean builder output files ==============
-rd /S /Q "builder\Release"
-rd /S /Q "builder\x64"
+echo =================== generate test dll ====================
+MSBuild.exe Gleam-RT.sln /t:test_dll /p:Configuration=Release /p:Platform=x86
+MSBuild.exe Gleam-RT.sln /t:test_dll /p:Configuration=Release /p:Platform=x64
+copy Release\test_dll.dll     dist\GleamRT_x86.dll
+copy x64\Release\test_dll.dll dist\GleamRT_x64.dll
+
+echo =================== clean output files ===================
 rd /S /Q "Release"
 rd /S /Q "x64"
+rd /S /Q "builder\Release"
+rd /S /Q "builder\x64"
+rd /S /Q "tool\test_dll\Release"
+rd /S /Q "tool\test_dll\x64"
 
 echo ================ generate assembly module ================
 go run dump.go
 
-echo ===================== test shellcode =====================
+echo ================== test runtime package ==================
 call test.bat
 
 echo ==========================================================

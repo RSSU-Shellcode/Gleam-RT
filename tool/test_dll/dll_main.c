62e59b90ad994710ed4c68239b510fd696d451c7
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-09 14:48:23 +0800

    test_dll/dll_main.c: add DllMain for placeholder, add Init procedure.

---
+++ tool/test_dll/dll_main.c
@@ -0,0 +1,22 @@
+﻿#include "c_types.h"
+#include "win_types.h"
+#include "dll_kernel32.h"
+#include "runtime.h"
+
+#pragma comment(linker, "/ENTRY:DllMain")
+BOOL DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+    (void)hModule;
+    (void)dwReason;
+    (void)lpReserved;
+    return true;
+}
+
+BOOL Init(Runtime_Opts* opts)
+{
+    if (InitRuntime(opts) == NULL)
+    {
+        return false;
+    }
+    return true;
+}

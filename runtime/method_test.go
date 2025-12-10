3a790edaad706efba3f063308839469fb0191d50
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-10 12:51:47 +0800

    method_test.go: implement init and TestMain for switch test dll.

--- runtime/method_test.go
+++ runtime/method_test.go
@@ -1,3 +1,50 @@
 //go:build windows
 
 package gleamrt
+
+import (
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows"
+)
+
+func init() {
+	var src string
+	switch runtime.GOARCH {
+	case "386":
+		src = "../dist/GleamRT_x86.dll"
+	case "amd64":
+		src = "../dist/GleamRT_x64.dll"
+	}
+	dll, err := os.ReadFile(src)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("GleamRT.dll", dll, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	err := windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+	err = os.Remove("GleamRT.dll")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(code)
+}
+
+func TestInit(t *testing.T) {
+	err := Init(nil)
+	require.NoError(t, err)
+}

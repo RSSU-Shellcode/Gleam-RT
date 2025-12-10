7a515bd8e619f5cd6aef2188eca9bf3d4ad63500
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-10 12:42:42 +0800

    test_dll/dll_main.c: add initialized for call RT_Exit after DLL_PROCESS_DETACH.

--- tool/test_dll/dll_main.c
+++ tool/test_dll/dll_main.c
@@ -1,15 +1,34 @@
 ﻿#include "c_types.h"
 #include "win_types.h"
 #include "dll_kernel32.h"
+#include "pe_image.h"
+#include "errno.h"
 #include "runtime.h"
+
+bool initialized = false;
 
 #pragma comment(linker, "/ENTRY:DllMain")
 BOOL DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
 {
+    switch (dwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        return true;
+    case DLL_PROCESS_DETACH:
+        if (!initialized)
+        {
+            return true;
+        }
+        return RT_Exit() == NO_ERROR;
+    case DLL_THREAD_ATTACH:
+        return true;
+    case DLL_THREAD_DETACH:
+        return true;
+    default:
+        return false;
+    }
     (void)hModule;
-    (void)dwReason;
     (void)lpReserved;
-    return true;
 }
 
 BOOL Init(Runtime_Opts* opts)
@@ -18,5 +37,6 @@ BOOL Init(Runtime_Opts* opts)
     {
         return false;
     }
+    initialized = true;
     return true;
 }

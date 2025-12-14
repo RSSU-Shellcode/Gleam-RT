6fce63de3d0284a238fbccd5b9a41181ec01a069
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-14 12:56:59 +0800

    argument/argument_test.go: add init, TestMain and TestGetValue.

--- runtime/argument/argument_test.go
+++ runtime/argument/argument_test.go
@@ -1,3 +1,70 @@
 //go:build windows
 
 package argument
+
+import (
+	"os"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"golang.org/x/sys/windows"
+
+	"github.com/RSSU-Shellcode/Gleam-RT/runtime"
+)
+
+func init() {
+	var src string
+	switch runtime.GOARCH {
+	case "386":
+		src = "../../dist/GleamRT_x86.dll"
+	case "amd64":
+		src = "../../dist/GleamRT_x64.dll"
+	}
+	dll, err := os.ReadFile(src)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("GleamRT.dll", dll, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	err := gleamrt.Initialize(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+
+	// must free twice for runtime package
+	err = windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+	err = windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Remove("GleamRT.dll")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(code)
+}
+
+// reference: script/args_gen.go
+
+func TestGetValue(t *testing.T) {
+	data, exist := GetValue(0)
+	if !exist {
+		t.Fatal("argument 0 is not exists")
+	}
+
+	expected := []byte{0x78, 0x56, 0x34, 0x12}
+	require.Equal(t, expected, data)
+}

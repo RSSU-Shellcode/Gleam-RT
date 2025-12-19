000ebc53094c44d1a365a570e6aab747cf47db73
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-19 14:15:58 +0800

    detector/detector.go: add type Status struct, improve GetStatus.

--- runtime/detector/detector.go
+++ runtime/detector/detector.go
@@ -17,6 +17,18 @@ var (
 	procStatus = modGleamRT.NewProc("DT_Status")
 )
 
+// Status contains detector status.
+type Status struct {
+	IsEnabled        bool  `toml:"is_enabled"         json:"is_enabled"`
+	HasDebugger      bool  `toml:"has_debugger"       json:"has_debugger"`
+	HasMemoryScanner bool  `toml:"has_memory_scanner" json:"has_memory_scanner"`
+	InSandbox        bool  `toml:"in_sandbox"         json:"in_sandbox"`
+	InVirtualMachine bool  `toml:"in_virtual_machine" json:"in_virtual_machine"`
+	InEmulator       bool  `toml:"in_emulator"        json:"in_emulator"`
+	IsAccelerated    bool  `toml:"is_accelerated"     json:"is_accelerated"`
+	SafeRank         int32 `toml:"safe_rank"          json:"safe_rank"`
+}
+
 // Detect is used to detect current environment.
 func Detect() error {
 	ret, _, err := procDetect.Call()
@@ -27,13 +39,23 @@ func Detect() error {
 	return nil
 }
 
-// Status is used to get detector status.
-func Status() (*metric.DTStatus, error) {
+// GetStatus is used to get detector status.
+func GetStatus() (*Status, error) {
 	var status metric.DTStatus
 	ret, _, err := procStatus.Call(uintptr(unsafe.Pointer(&status)))
 	if ret == 0 {
 		en := uintptr(err.(syscall.Errno))
 		return nil, fmt.Errorf("failed to call detector.Status: 0x%08X", en)
 	}
-	return &status, nil
+	s := Status{
+		IsEnabled:        status.IsEnabled.ToBool(),
+		HasDebugger:      status.HasDebugger.ToBool(),
+		HasMemoryScanner: status.HasMemoryScanner.ToBool(),
+		InSandbox:        status.InSandbox.ToBool(),
+		InVirtualMachine: status.InVirtualMachine.ToBool(),
+		InEmulator:       status.InEmulator.ToBool(),
+		IsAccelerated:    status.IsAccelerated.ToBool(),
+		SafeRank:         status.SafeRank,
+	}
+	return &s, nil
 }

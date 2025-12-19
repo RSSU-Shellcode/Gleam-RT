f4175883b3258e4db81195f9838c19ca28919d65
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-19 14:08:00 +0800

    detector/detector_test.go: add init and TestMain for prepare tests.

---
+++ runtime/detector/detector_test.go
@@ -0,0 +1,55 @@
+package detector
+
+import (
+	"os"
+	"runtime"
+	"testing"
+
+	"golang.org/x/sys/windows"
+
+	"github.com/RSSU-Shellcode/Gleam-RT/runtime"
+)
+
+func init() {
+	var src string
+	switch runtime.GOARCH {
+	case "386":
+		src = "../../dist/GleamRT_x86.dll"
+	case "amd64":
+		src = "../../dist/GleamRT_x64.dll"
+	}
+	dll, err := os.ReadFile(src)
+	if err != nil {
+		panic(err)
+	}
+	err = os.WriteFile("GleamRT.dll", dll, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestMain(m *testing.M) {
+	err := gleamrt.Initialize(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	code := m.Run()
+
+	// must free twice for runtime package
+	err = windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+	err = windows.FreeLibrary(windows.Handle(modGleamRT.Handle()))
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.Remove("GleamRT.dll")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Exit(code)
+}
